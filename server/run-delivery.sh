4176452a7a664a1310ca2816971041fc60c66d71
Bamacharan Kundu <bamachrn@gmail.com>
2016-03-11 08:43:25 +0000

    Added certificate file and docker json

--- server/run-delivery.sh
+++ server/run-delivery.sh
@@ -3,15 +3,13 @@
 function _() {
     echo "==> $@"
 }
-
-if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
-  ll /var/run/secrets/openshift.io/push/
-  cp /var/run/secrets/openshift.io/push/.dockercfg /root/.dockercfg
-fi
+TOKEN=`cat /var/run/secrets/kubernetes.io/serviceaccount/token`
 
 if [ "${TARGET_REGISTRY}" == "" ];then
     TARGET_REGISTRY=${OUTPUT_REGISTRY}
 fi
+
+docker login -u serviceaccount -p ${TOKEN} -e serviceaccount@example.org ${OUTPUT_REGISTRY}
 
 FULL_FROM=${OUTPUT_REGISTRY}/`python -c 'import json, os; print json.loads(os.environ["BUILD"])["metadata"]["namespace"]'`/${FROM}
 FULL_TO=${TARGET_REGISTRY}/${TARGET_NAMESPACE}/${TO}
@@ -20,6 +18,15 @@ _ "Pulling RC image (${FROM})"
 docker pull ${FULL_FROM}
 docker tag ${FULL_FROM} ${FULL_TO}
 
+wget dev-32-43.lon1.centos.org/certs/dev-32-43.lon1.centos.org.crt
+
+mkdir -p /etc/docker/certs.d/dev-32-43.lon1.centos.org/
+
+cp ./dev-32-43.lon1.centos.org.crt /etc/docker/certs.d/dev-32-43.lon1.centos.org/ca.crt
+
+#if [[ -d /var/run/secrets/openshift.io/push ]] && [[ ! -e /root/.dockercfg ]]; then
+#  cp /var/run/secrets/openshift.io/push/ca.crt /etc/docker/certs.d/dev-32-43.lon1.centos.org/ca.crt
+#fi
 _ "Pushing final image (${FULL_TO})"
 docker push ${FULL_TO}
 
