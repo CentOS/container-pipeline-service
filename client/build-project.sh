4cc304e6531d1ffa317fba259ce0cf2d5925b6b8
Bamacharan Kundu <bamachrn@gmail.com>
2016-03-31 03:27:34 +0000

    Added oc login and changed template

--- client/build-project.sh
+++ client/build-project.sh
@@ -9,6 +9,7 @@ function usage() {
     echo "   TAG       Name of the resulting image (image will be named NAME/TAG:latest)"
     echo "   REPO_URL  URL of project repository containing Dockerfile"
     echo "   REPO_BUILD_PATH  Relative path to the Dockerfile in the repository"
+    echo "   NOTIFY_EMAIL  Email ID to be notified after successful build"
     exit 0
 }
 
@@ -20,11 +21,13 @@ NAME=$1
 TAG=$2
 REPO=$3
 REPO_BUILD_PATH=$4
+NOTIFY_EMAIL=$5
 
 [ "${NAME}" == "" ] || [ "${NAME}" == "-h" ] || [ "${NAME}" == "--help" ] && usage
 [ "${TAG}" == "" ] && usage
 [ "${REPO}" == "" ] && usage
 [ "${REPO_BUILD_PATH}" == "" ] && usage
+[ "${NOTIFY_EMAIL}" == "" ] && usage
 
 
 CWD=`dirname $0`
@@ -40,7 +43,7 @@ for t in $(echo "build bc is"); do
   _oc ${NS} delete $t $(oc get $t -l template=cccp-service --no-headers | awk '{print $1}')
 done
 _oc ${NS} get --no-headers  -f $CWD/template.json && oc replace -f $CWD/template.json || oc ${NS} create -f $CWD/template.json
-_oc ${NS} process cccp-service -v SOURCE_REPOSITORY_URL=${REPO},TARGET_NAMESPACE=${NAME},TAG=${TAG},REPO_BUILD_PATH=${REPO_BUILD_PATH} | oc ${NS} create -f -
+_oc ${NS} process cccp-service -v SOURCE_REPOSITORY_URL=${REPO},TARGET_NAMESPACE=${NAME},TAG=${TAG},REPO_BUILD_PATH=${REPO_BUILD_PATH},NOTIFY_EMAIL=${NOTIFY_EMAIL} | oc ${NS} create -f -
 
 IP="bs.pco.centos.org"
 #$(ip -f inet addr show eth1 2> /dev/null | grep 'inet' | awk '{ print $2}' | sed 's#/.*##')
