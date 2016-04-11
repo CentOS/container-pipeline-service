610ba431aae9dfb5797a6a39060617359a250926
Bamacharan Kundu <bamachrn@gmail.com>
2016-04-11 06:31:59 +0000

    Fixed multiuser same project name issue

--- client/build-project.sh
+++ client/build-project.sh
@@ -31,19 +31,21 @@ NOTIFY_EMAIL=$5
 
 
 CWD=`dirname $0`
-NS="--namespace ${NAME}"
+NS="--namespace ${NAME}-${TAG}"
 echo "==> login to Openshift server"
-oc login https://172.29.32.42:8443 -u test-admin -p test --certificate-authority=./ca.crt
+oc login https://172.29.32.53:8443 -u test-admin -p test --certificate-authority=./ca.crt
 
 echo "==>creating new project"
-oc new-project ${NAME} --display-name=${NAME}
+oc new-project ${NAME}-${TAG} --display-name=${NAME}-${TAG}
 
-echo "==> Uploading template to OpenShift"
-for t in $(echo "build bc is"); do
-  _oc ${NS} delete $t $(oc get $t -l template=cccp-service --no-headers | awk '{print $1}')
-done
+#echo "==> Uploading template to OpenShift"
+#for t in $(echo "build bc is"); do
+#  _oc ${NS} delete $t $(oc get $t -l template=cccp-service --no-headers | awk '{print $1}')
+#done
+sed -i.bak s/cccp-service/${NAME}-${TAG}/g $CWD/template.json
+
 _oc ${NS} get --no-headers  -f $CWD/template.json && oc replace -f $CWD/template.json || oc ${NS} create -f $CWD/template.json
-_oc ${NS} process cccp-service -v SOURCE_REPOSITORY_URL=${REPO},TARGET_NAMESPACE=${NAME},TAG=${TAG},REPO_BUILD_PATH=${REPO_BUILD_PATH},NOTIFY_EMAIL=${NOTIFY_EMAIL} | oc ${NS} create -f -
+_oc ${NS} process ${NAME}-${TAG} -v SOURCE_REPOSITORY_URL=${REPO},TARGET_NAMESPACE=${NAME},TAG=${TAG},REPO_BUILD_PATH=${REPO_BUILD_PATH},NOTIFY_EMAIL=${NOTIFY_EMAIL} | oc ${NS} create -f -
 
 IP="bs.pco.centos.org"
 #$(ip -f inet addr show eth1 2> /dev/null | grep 'inet' | awk '{ print $2}' | sed 's#/.*##')
@@ -51,3 +53,8 @@ IP="bs.pco.centos.org"
 BUILD=$(_oc ${NS} start-build build)
 
 [ $? -eq 0 ] && echo -e "Build ${BUILD} started.\nYou can watch builds progress at https://${IP}:8443/console/project/${NAME}/browse/builds"
+
+echo "==> Restoring the default template"
+rm -rf $CWD/template.json
+mv $CWD/template.json.bak $CWD/template.json
+
