40ff7c19c0d8be1ba100abfb529f2d28de3045be
mohammedzee1000 <mohammed.zee1000@gmail.com>
2016-06-08 15:44:18 +0530

    Added ability to override index file, instead of specifying git repo, if needed.

--- testindexentry.py
+++ testindexentry.py
@@ -7,6 +7,7 @@ from pprint import PrettyPrinter
 from subprocess import check_call, CalledProcessError, call
 from time import sleep
 import argparse
+import shutil
 
 import yaml
 
@@ -79,41 +80,66 @@ class StaticHandler:
         return success
 
     @staticmethod
-    def initialize_all(customindex=False):
+    def initialize_all(customindex=False, forceclone=False):
 
         # If the test dir does not exist, create it with all permissions
-        if not os.path.exists(TestConsts.testdir):
-            os.mkdir(TestConsts.testdir, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        if not os.path.exists(TestGlobals.testdir):
+            os.mkdir(TestGlobals.testdir, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+            os.mkdir(TestGlobals.testdir + "/index")
 
         if not customindex:
             # Check if the index repo exists, if it does, fetch the updates.
-            if os.path.exists(TestConsts.testdir + "/index"):
+            if not forceclone and os.path.exists(TestGlobals.testdir + "/index"):
+
                 StaticHandler.print_msg(MessageType.info, "Updating index repo...")
                 currdir = os.getcwd()
-                os.chdir(TestConsts.testdir + "/index")
+                os.chdir(TestGlobals.testdir + "/index")
                 cmd = ["git", "fetch", "--all"]
                 os.chdir(currdir)
 
             # If not, clone it
             else:
+
                 StaticHandler.print_msg(MessageType.info, "Cloning index repo...")
                 # Clone the index repo
-                cmd = ["git", "clone", TestConsts.indexgit, TestConsts.testdir + "/index"]
-                call(cmd)
-                currdir = os.getcwd()
-                os.chdir(TestConsts.testdir + "/index")
-                cmd = ["git", "fetch", "--all"]
-                os.chdir(currdir)
-                sleep(5)
+                cmd = ["git", "clone", TestGlobals.indexgit, TestGlobals.testdir + "/index"]
 
-        TestConsts.indxfile = TestConsts.testdir + "/index" + "/index.yml"
+                if StaticHandler.execcmd(cmd):
+                    currdir = os.getcwd()
+                    os.chdir(TestGlobals.testdir + "/index")
+                    cmd = ["git", "fetch", "--all"]
+                    os.chdir(currdir)
+
+                else:
+                    StaticHandler.print_msg(MessageType.error, "Could not clone the git index repo,"
+                                                               " please check the url")
+                    sys.exit(900)
+        else:
+
+            if os.path.exists(TestGlobals.indxfile):
+
+                if not os.path.isabs(TestGlobals.indxfile):
+
+                    TestGlobals.indxfile = os.path.abspath(TestGlobals.indxfile)
+
+                StaticHandler.print_msg(MessageType.info, "Copying index to dump location.")
+                shutil.copy2(TestGlobals.indxfile, TestGlobals.testdir + "/index" + "/index.yml")
+
+            else:
+
+                StaticHandler.print_msg(MessageType.error, "The index file you specified, does not exist.")
+                sys.exit(900)
+
+            sleep(5)
+
+        TestGlobals.indxfile = TestGlobals.testdir + "/index" + "/index.yml"
 
         print
 
         return
 
 
-class TestConsts:
+class TestGlobals:
     """Contains constants being used by the script"""
 
     def __init__(self):
@@ -147,12 +173,12 @@ class TestEntry:
             gitpath += "/"
 
         fnm = tid + "_" + appid + "_" + jobid
-        self._gitReposlocation = TestConsts.testdir + "/repos"
+        self._gitReposlocation = TestGlobals.testdir + "/repos"
 
-        if not os.path.exists(TestConsts.testdir + "/tests"):
-            os.mkdir(TestConsts.testdir + "/tests")
+        if not os.path.exists(TestGlobals.testdir + "/tests"):
+            os.mkdir(TestGlobals.testdir + "/tests")
 
-        self._testLogFile = TestConsts.testdir + "/tests/" + fnm + ".log"
+        self._testLogFile = TestGlobals.testdir + "/tests/" + fnm + ".log"
 
         self._id = tid
         self._appId = appid
@@ -257,7 +283,7 @@ class TestEntry:
             else:
 
                 StaticHandler.print_msg(MessageType.error, "Failed to clone repo, skipping...", self)
-                TestConsts.exitcode += 1
+                TestGlobals.exitcode += 1
                 success = False
 
         self._testData["tests"]["clone"] = success
@@ -312,13 +338,13 @@ class TestEntry:
             else:
 
                 StaticHandler.print_msg(MessageType.error, "Job Ids don't match, skipping...", self)
-                TestConsts.exitcode += 1
+                TestGlobals.exitcode += 1
                 return
 
         else:
 
             StaticHandler.print_msg(MessageType.error, "Missing compulsory key, job-id in cccp.yml file...", self)
-            TestConsts.exitcode += 1
+            TestGlobals.exitcode += 1
             return
 
         # * Validate for test-skip and test-script
@@ -350,7 +376,7 @@ class TestEntry:
                         StaticHandler.print_msg(MessageType.error,
                                                 "The specified test script does not exist, skipping...", self)
 
-                        TestConsts.exitcode += 1
+                        TestGlobals.exitcode += 1
                         return
 
                     else:
@@ -364,7 +390,7 @@ class TestEntry:
 
                     StaticHandler.print_msg(MessageType.error,
                                             "Test skip is reset, but test script is missing, skipping...", self)
-                    TestConsts.exitcode += 1
+                    TestGlobals.exitcode += 1
                     return
 
             # If test-skip is not reset, check if its set
@@ -378,7 +404,7 @@ class TestEntry:
             else:
 
                 StaticHandler.print_msg(MessageType.error, "Test skip is not reset or set, skipping...", self)
-                TestConsts.exitcode += 1
+                TestGlobals.exitcode += 1
                 return
 
         else:
@@ -397,7 +423,7 @@ class TestEntry:
             if not os.path.exists(buildscriptpath):
 
                 StaticHandler.print_msg(MessageType.error, "Could not find build script, skipping", self)
-                TestConsts.exitcode += 1
+                TestGlobals.exitcode += 1
                 return
 
             else:
@@ -466,6 +492,10 @@ class Tester:
         self._parser.add_argument("-g", "--indexgit", help="Specify a custom git containing the index.yml",
                                   metavar=('GITURL'), nargs=1, action="store")
 
+        self._parser.add_argument("-c", "--customindex", help="Specify a custom index file, stored locally."
+                                                              "DO NOT use with -g", metavar=('INDEXPATH'), nargs=1,
+                                  action="store")
+
         return
 
     def run(self, args):
@@ -474,43 +504,60 @@ class Tester:
         t = self._i
 
         cmdargs = self._parser.parse_args()
+        initialized = False
 
+        if cmdargs.indexgit is not None and cmdargs.customindex is not None:
+            StaticHandler.print_msg(MessageType.error, "Error, -g and -c are mutually exclusive, specify either one")
+            sys.exit(900)
+
+        # If dump directory is specified, update the globals
         if cmdargs.dumpdirectory is not None:
 
             dpth = cmdargs.dumpdirectory[0]
 
             if not os.path.isabs(dpth):
                 dpth = os.path.abspath(dpth)
-                TestConsts.testdir = dpth
+                TestGlobals.testdir = dpth
 
             if not os.path.exists(dpth):
                 StaticHandler.print_msg(MessageType.error, "Invalid path specified or does not exist")
                 sys.exit(900)
 
+        # If index git is specified, update globals
         if cmdargs.indexgit is not None:
 
             gurl = cmdargs.indexgit[0]
-            TestConsts.indexgit = gurl
+            TestGlobals.indexgit = gurl
+            StaticHandler.initialize_all(forceclone=True)
+            initialized = True
 
-        # FIXME: add entry for
-        StaticHandler.initialize_all()
+        # If customindex is specified, initialize appropriately
+        if cmdargs.customindex is not None:
+
+            cind = cmdargs.customindex[0]
+            TestGlobals.indxfile = cind
+            StaticHandler.initialize_all(customindex=True)
+            initialized = True
+
+        if not initialized:
+            StaticHandler.initialize_all()
 
         resultset = {
             "Projects": []
         }
 
         # Checks if the index file exists. Required for any tests to proceed
-        if os.path.exists(TestConsts.indxfile):
+        if os.path.exists(TestGlobals.indxfile):
 
             # read in data from index file.
-            with open(TestConsts.indxfile) as indexfile:
+            with open(TestGlobals.indxfile) as indexfile:
                 indexentries = yaml.load(indexfile)
 
                 i = 0
 
             # If exit code is set set the value :
             if cmdargs.exitcode is True:
-                TestConsts.giveexitcode = True
+                TestGlobals.giveexitcode = True
 
             # If no index entries or test entries were specified do everything
             if cmdargs.indexentry is None and cmdargs.testentry is None:
@@ -698,15 +745,15 @@ def mainf():
     rs = Tester().run(sys.argv)
 
     print "\nTests completed\n"
-    resultsfile = TestConsts.testdir + "/results.yml"
+    resultsfile = TestGlobals.testdir + "/results.yml"
 
     with open(resultsfile, "w") as resfile:
         yaml.dump(rs, resfile, default_flow_style=False)
 
     print "\nYou can view the test results at " + resultsfile + "\n"
 
-    if TestConsts.giveexitcode:
-        sys.exit(TestConsts.exitcode)
+    if TestGlobals.giveexitcode:
+        sys.exit(TestGlobals.exitcode)
 
 
 if __name__ == '__main__':
