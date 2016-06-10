3908fa849d6ef38717c6d4a0161305c68cf1df53
Bamacharan Kundu <bamachrn@gmail.com>
2016-06-10 09:54:31 +0530

    Fixed mail failed subject for status mails

--- mailmq/mail-config.sh
+++ mailmq/mail-config.sh
@@ -3,5 +3,5 @@ export REPLYTO=container-build@centos.org
 mkfifo /var/spool/postfix/public/pickup
 postfix start
 sleep 30
-echo $1 | mail -r container-build-report@centos.org -s $1 $2
+echo $1 | mail -r container-build-report@centos.org -s "container build failed" $2
 sleep 30
