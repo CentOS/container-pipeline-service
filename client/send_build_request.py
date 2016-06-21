acc27cbcfe4d225358f73f9cf8cb15d112aff168
Bamacharan Kundu <bamachrn@gmail.com>
2016-06-21 10:38:51 +0530

    Added build queue for making build sequential

---
+++ client/send_build_request.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+import beanstalkc
+import json
+
+print "Getting build details from jenkins"
+var build_details = {}
+build_details['name'] = sys.argv[1]
+build_details['tag']  = sys.argv[2]
+
+print "Pushing bild details in the tube"
+bs = beanstalkc.Connection(host="openshift")
+bs.use("start_build")
+bs.put(json.dumps(build_details))
+print "build details is pushed to tube"
