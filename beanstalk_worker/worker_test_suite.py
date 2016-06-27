92c53747a9e4db43cd6652ea0765f94ae2ccc15a
Dharmit Shah <shahdharmit@gmail.com>
2016-06-27 16:58:35 +0530

    Remote try..except so that job doesn't get deleted in case of failure

--- beanstalk_worker/worker_test_suite.py
+++ beanstalk_worker/worker_test_suite.py
@@ -48,74 +48,71 @@ def split_logs_to_packages(list_logs):
 def test_job_data(job_data):
     msg = ""
     logs = ""
-    try:
-        logger.log(level=logging.INFO, msg="Received job data from tube")
-        logger.log(level=logging.INFO, msg="Job data: %s" % job_data)
+    logger.log(level=logging.INFO, msg="Received job data from tube")
+    logger.log(level=logging.INFO, msg="Job data: %s" % job_data)
 
-        # if job_data.get("tag") != None:
-        #     image_full_name = job_data.get("name") + ":" + \
-        #         job_data.get("image_tag")
-        # else:
-        #     image_full_name = job_data.get("name")
+    # if job_data.get("tag") != None:
+    #     image_full_name = job_data.get("name") + ":" + \
+    #         job_data.get("image_tag")
+    # else:
+    #     image_full_name = job_data.get("name")
 
-        image_full_name = job_data.get('name').split(":")[0] + ":" + \
-            job_data.get("tag")
+    image_full_name = job_data.get('name').split(":")[0] + ":" + \
+        job_data.get("tag")
 
-        logger.log(level=logging.INFO, msg="Pulling image %s" % image_full_name)
-        pull_data = conn_c7.pull(
-            repository=image_full_name
-        )
+    logger.log(level=logging.INFO, msg="Pulling image %s" % image_full_name)
+    pull_data = conn_c7.pull(
+        repository=image_full_name
+    )
 
-        if 'error' in pull_data:
-            logger.log(level=logging.FATAL, msg="Couldn't pull requested image")
-            logger.log(level=logging.FATAL, msg=pull_data)
-            sys.exit(1)
+    if 'error' in pull_data:
+        logger.log(level=logging.FATAL, msg="Couldn't pull requested image")
+        logger.log(level=logging.FATAL, msg=pull_data)
+        sys.exit(1)
 
-        logger.log(level=logging.INFO,
-                   msg="Creating container for image %s" % image_full_name)
+    logger.log(level=logging.INFO,
+               msg="Creating container for image %s" % image_full_name)
 
-        container = conn_c7.create_container(image=image_full_name,
-                                             command="yum -q check-update")
-        logger.log(level=logging.INFO,
-                   msg="Created container with ID: %s" % container.get('Id'))
+    container = conn_c7.create_container(image=image_full_name,
+                                         command="yum -q check-update")
+    logger.log(level=logging.INFO,
+               msg="Created container with ID: %s" % container.get('Id'))
 
-        conn_c7.start(container=container.get('Id'))
+    conn_c7.start(container=container.get('Id'))
 
-        logger.log(level=logging.INFO,
-                   msg="Started container with ID: %s" % container.get('Id'))
+    logger.log(level=logging.INFO,
+               msg="Started container with ID: %s" % container.get('Id'))
 
-        time.sleep(10)
-        logs = conn_c7.logs(container=container.get('Id'))
-        if logs != "":
-            list_logs = logs.split("\n")
-            package_list = split_logs_to_packages(list_logs)
-            msg = "Image has package updates. Recommend updating the image"
-            logger.log(level=logging.WARN, msg=msg)
-            logger.log(level=logging.INFO, msg=str(package_list))
+    time.sleep(10)
+    logs = conn_c7.logs(container=container.get('Id'))
+    if logs != "":
+        list_logs = logs.split("\n")
+        package_list = split_logs_to_packages(list_logs)
+        msg = "Image has package updates. Recommend updating the image"
+        logger.log(level=logging.WARN, msg=msg)
+        logger.log(level=logging.INFO, msg=str(package_list))
 
-        logger.log(level=logging.INFO, msg="Stopping test container")
-        conn_c7.stop(container=container.get('Id'))
+    logger.log(level=logging.INFO, msg="Stopping test container")
+    conn_c7.stop(container=container.get('Id'))
 
-        logger.log(level=logging.INFO, msg="Removing the test container")
-        conn_c7.remove_container(container=container.get('Id'), force=True)
+    logger.log(level=logging.INFO, msg="Removing the test container")
+    conn_c7.remove_container(container=container.get('Id'), force=True)
 
-        logger.log(level=logging.INFO, msg="Removing the image %s" % image_full_name)
-        conn_c7.remove_image(image=image_full_name, force=True)
+    logger.log(level=logging.INFO, msg="Removing the image %s" % image_full_name)
+    conn_c7.remove_image(image=image_full_name, force=True)
 
-        logger.log(level=logging.INFO, msg="Finished test...")
+    logger.log(level=logging.INFO, msg="Finished test...")
 
-        if msg != "" and logs != "":
-            d = {"image": image_full_name, "msg": msg, "logs": package_list}
-        else:
-            d = {"image": image_full_name, "msg": "No updates required", "logs": ""}
-        bs.use("start_deliver")
-        jid = bs.put(json.dumps(d))
-        logger.log(
-            level=logging.INFO,
-            msg="Put job on 'start_deliver' tube with id: %d" % jid
-        )
-    except Exception as e:
-        logger.log(level=logging.CRITICAL, msg=e.message)
+    if msg != "" and logs != "":
+        d = {"image": image_full_name, "msg": msg, "logs": package_list}
+    else:
+        d = {"image": image_full_name, "msg": "No updates required", "logs": ""}
+    bs.use("start_deliver")
+    jid = bs.put(json.dumps(d))
+    logger.log(
+        level=logging.INFO,
+        msg="Put job on 'start_deliver' tube with id: %d" % jid
+    )
 
 bs = beanstalkc.Connection(host="openshift")
 bs.watch("start_test")
