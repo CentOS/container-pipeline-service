3c4cef8e338bb54e63ee02b2c29e13cec9769dfb
Bamacharan Kundu <bamachrn@gmail.com>
2016-07-11 15:37:52 +0530

    Added notify worker

---
+++ server/worker_notify_user.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import beanstalkc
+import json
+import subprocess
+import re
+import time
+
+bs = beanstalkc.Connection(host="openshift")
+bs.watch("start_build")
+bs.use("failed_build")
+
+while True:
+  print "listening to start_build tube"
+  job = bs.reserve()
+  jobid = job.jid
+  job_details = json.loads(job.body) 
+  
+  print "==> Retrieving namespace"
+  to_mail = job_details['to_mail']
+  msg = job_details['msg']
+  attachment = job_details['attachment']
+  
+  print "==> Login to openshift server"
+  command = "/mail_service/mail-config.sh"
+  subprocess.call([command,msg,to_mail])
+  
+  job.delete()
