33522b8a7008b7ae815be112227de9f29d559797
Bamacharan Kundu <bamachrn@gmail.com>
2016-07-12 13:25:26 +0530

    Added git branch to be built

--- client/build-project.sh
+++ client/build-project.sh
@@ -8,8 +8,9 @@ function usage() {
     echo "   NAME      Name of the project/namespace"
     echo "   TAG       Name of the resulting image (image will be named NAME/TAG:latest)"
     echo "   REPO_URL  URL of project repository containing Dockerfile"
+    echo "   REPO_BRANCH Branch of the repo to be built"
     echo "   REPO_BUILD_PATH  Relative path to the Dockerfile in the repository"
-    echo "   DOCKERFILE_NAME  Name for the dockerfile to be built"
+    echo "   TARGET_FILE  Name for the dockerfile to be built"
     echo "   NOTIFY_EMAIL  Email ID to be notified after successful build"
     echo "   DEPENDS_ON  Dependency list for the current image"
     exit 0
@@ -22,16 +23,18 @@ function _oc() {
 NAME=$1
 TAG=$2
 REPO=$3
-REPO_BUILD_PATH=$4
-DOCKERFILE_NAME=$5
-NOTIFY_EMAIL=$6
-DEPENDS_ON=$7
+REPO_BRANCH=$4
+REPO_BUILD_PATH=$5
+TARGET_FILE=$6
+NOTIFY_EMAIL=$7
+DEPENDS_ON=$8
 
 [ "${NAME}" == "" ] || [ "${NAME}" == "-h" ] || [ "${NAME}" == "--help" ] && usage
 [ "${TAG}" == "" ] && usage
 [ "${REPO}" == "" ] && usage
+[ "${REPO_BRANCH}" == "" ] && usage
 [ "${REPO_BUILD_PATH}" == "" ] && usage
-[ "${DOCKERFILE_NAME}" == "" ] && usage
+[ "${TARGET_FILE}" == "" ] && usage
 [ "${NOTIFY_EMAIL}" == "" ] && usage
 [ "${DEPENDS_ON}" == "" ] && usage
 
@@ -52,7 +55,7 @@ for t in $(echo "build bc is"); do
 done
 
 _oc ${NS} get --no-headers  -f $CWD/template.json && oc replace -f $CWD/template.json || oc ${NS} create -f $CWD/template.json
-_oc ${NS} process ${NAME}-${TAG} -v SOURCE_REPOSITORY_URL=${REPO},TARGET_NAMESPACE=${NAME},TAG=${TAG},REPO_BUILD_PATH=${REPO_BUILD_PATH},DOCKERFILE_NAME=${DOCKERFILE_NAME},NOTIFY_EMAIL=${NOTIFY_EMAIL} | oc ${NS} create -f -
+_oc ${NS} process ${NAME}-${TAG} -v SOURCE_REPOSITORY_URL=${REPO},REPO_BRANCH=${REPO_BRANCH},TARGET_NAMESPACE=${NAME},TAG=${TAG},REPO_BUILD_PATH=${REPO_BUILD_PATH},TARGET_FILE=${TARGET_FILE},NOTIFY_EMAIL=${NOTIFY_EMAIL} | oc ${NS} create -f -
 
 IP=$(ip -f inet addr show eth1 2> /dev/null | grep 'inet' | awk '{ print $2}' | sed 's#/.*##')
 
