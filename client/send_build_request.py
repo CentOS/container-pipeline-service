4ef8098c539f271585657297f61b71b8d09be24c
Bamacharan Kundu <bamachrn@gmail.com>
2016-07-12 17:38:16 +0530

    Send the build request jobs to master tube with action start_build

--- client/send_build_request.py
+++ client/send_build_request.py
@@ -9,9 +9,10 @@ build_details = {}
 build_details['name'] = sys.argv[1]
 build_details['tag']  = sys.argv[2]
 build_details['depends_on'] = sys.argv[3]
+build_details['action'] = "start_build"
 
 print "Pushing bild details in the tube"
 bs = beanstalkc.Connection(host="openshift")
-bs.use("start_build")
+bs.use("master_tube")
 bs.put(json.dumps(build_details))
-print "build details is pushed to tube"
+print "build details is pushed to master tube"
