a7ec5f86c2c9cd0bae1fffdd17d355341594a070
Bamacharan Kundu <bamachrn@gmail.com>
2016-09-29 13:37:00 +0530

    Validated scripts exists or not

--- server/cccp_reader.py
+++ server/cccp_reader.py
@@ -12,7 +12,11 @@ def main():
   curr_dir = os.getcwd()
  
   print "==> Saving scripts for build process"
-  
+ 
+  got_build_script = 0;
+  got_test_script = 0;
+  got_delivery_script = 0;
+
   for key, value in cccp_yml.iteritems():
     if(key == "job-id"):
       job_id = cccp_yml["job-id"]
@@ -22,22 +26,29 @@ def main():
     
     if(key == "build-script"):
       build_script_path = cccp_yml["build-script"]
-      os.symlink(os.path.join(curr_dir,build_script_path),"/usr/bin/build_script")
-    else:
-      echo_scripts("/usr/bin/build_script","Build")
+      os.symlink(os.path.join(curr_dir,build_script_path),"/build_script")
+      got_build_script=1;
     
     if(key == "test-script"):
       test_script = cccp_yml["test-script"]
       if(test_skip != True):
-        os.symlink(os.path.join(curr_dir,test_script),"/usr/bin/test_script")
-    else:
-      echo_scripts("/usr/bin/test_script","Test")
+        os.symlink(os.path.join(curr_dir,test_script),"/test_script")
+        got_test_script=1;
     
     if(key == "delivery-script"):
       delivery_script = cccp_yml["delivery-script"]
-      os.symlink(os.path.join(curr_dir,delivery_script),"/usr/bin/delivery_script")
-    else:
-      echo_scripts("/usr/bin/delivery_script","Delivery")
+      os.symlink(os.path.join(curr_dir,delivery_script),"/delivery_script")
+      got_delivery_script=1;
+
+  if(got_build_script == 0)
+    echo_scripts("/build_script","Build ");
+
+  if(got_test_script == 0)
+    echo_scripts("/test_scripts","Test ");
+
+  if(got_delivery_script == 0)
+    echo_scripts("/delivery_scripts","Delivery ");
+
 
   print "==> scripts saved"
 
