af3bbefd9d1b6627b1354c949f6ee504af1e361e
Bamacharan Kundu <bamachrn@gmail.com>
2016-09-30 11:52:00 +0530

    Shifted failed notifier to worker and added logs

--- beanstalk_worker/worker_start_build.py
+++ beanstalk_worker/worker_start_build.py
@@ -31,88 +31,94 @@ config_path = "/".join(sys.argv[0].split("/")[:-1])
 DEBUG=1
 
 def debug_log(msg):
-  if DEBUG==1:
-    logger.log(level=logging.INFO,msg=msg)
+    if DEBUG==1:
+        logger.log(level=logging.INFO,msg=msg)
 
 def run_command(command):
-  p = Popen(command,bufsize=0,shell=True,stdout=PIPE,stderr=PIPE,stdin=PIPE)
-  p.wait()
-  out = p.communicate()
-  return out
+    p = Popen(command,bufsize=0,shell=True,stdout=PIPE,stderr=PIPE,stdin=PIPE)
+    p.wait()
+    out = p.communicate()
+    return out
+
+def notify_build_failure(namespace, notify_email, logs):
+    msg_details = {}
+    msg_details['action'] = 'notify_user'
+    msg_details['subject'] = "Container-build failed"+namespace
+    msg_details['msg'] = "Container build failed due to error in build or test steps. Pleae check attached logs"
+    msg_details['logs'] = logs
+    msg_details['to_mail'] = notify_email
+    bs.use('master_tube')
+    bs.put(json.dumps(msg_details))
 
 
 def start_build(job_details):
-  try:
-    debug_log(" Retrieving namespace")
-    name = job_details['name']
-    tag = job_details['tag']
-    depends_on = job_details['depends_on']
-  
-    #print "==> check dependencies are available or not"
-    #dependency_present = False
-    #if depends_on == None:
-    #  dependency_present = True
+    try:
+        debug_log(" Retrieving namespace")
+        name = job_details['name']
+        tag = job_details['tag']
+        depends_on = job_details['depends_on']
+        notify_email = job_details['notify_email']
 
-    #dc = DependencyChecker()
-    #while dependency_present != True
-    #  dependency_present = dc.checkdependencies(depends_on)
-    #  time.sleep(30)
+        debug_log("Login to openshift server")
+        command_login = "oc login https://openshift:8443 -u test-admin -p test --config="+config_path+"/node.kubeconfig --certificate-authority="+config_path+"/ca.crt"
+        out = run_command(command_login)
+        debug_log(out)
 
-    debug_log("Login to openshift server")
-    command_login = "oc login https://openshift:8443 -u test-admin -p test --config="+config_path+"/node.kubeconfig --certificate-authority="+config_path+"/ca.crt"
-    out = run_command(command_login)
-    debug_log(out)
+        debug_log(" change project to the desired one")
+        command_change_project = "oc project "+name+"-"+tag+" --config="+config_path+"/node.kubeconfig"
+        out = run_command(command_change_project)
+        debug_log(out)
 
-    debug_log(" change project to the desired one")
-    command_change_project = "oc project "+name+"-"+tag+" --config="+config_path+"/node.kubeconfig"
-    out = run_command(command_change_project)
-    debug_log(out)
-  
-    debug_log("start the build")
-    command_start_build = "oc --namespace "+name+"-"+tag+" start-build build --config="+config_path+"/node.kubeconfig"
-    out = run_command(command_start_build)
-    debug_log(out)
+        debug_log("start the build")
+        command_start_build = "oc --namespace "+name+"-"+tag+" start-build build --config="+config_path+"/node.kubeconfig"
+        out = run_command(command_start_build)
+        debug_log(out)
 
-    build_details = out[0].rstrip()
-    debug_log(build_details)
+        build_details = out[0].rstrip()
+        debug_log(build_details)
 
-    if build_details=="":
-      logger.log(level=logging.CRITICAL, msg="build could not be started as openshift is not reachable")
-      return 1
+        if build_details=="":
+            logger.log(level=logging.CRITICAL, msg="build could not be started as openshift is not reachable")
+            return 1
 
-    debug_log("build started is "+build_details)
+        debug_log("build started is "+build_details)
 
-    status_command = "oc get --namespace "+name+"-"+tag+" build/"+build_details+" --config="+config_path+"/node.kubeconfig|grep -v STATUS"
-    is_running = 1
+        status_command = "oc get --namespace "+name+"-"+tag+" build/"+build_details+" --config="+config_path+"/node.kubeconfig|grep -v STATUS"
+        is_running = 1
 
-    debug_log("Checking the build status")
-    while is_running >= 0:
-      status= run_command(status_command)[0].rstrip()
-      is_running = re.search("New|Pending|Running",status)
-      debug_log("current status: "+status)
-      time.sleep(30)
+        debug_log("Checking the build status")
+        while is_running >= 0:
+            status= run_command(status_command)[0].rstrip()
+            is_running = re.search("New|Pending|Running",status)
+            debug_log("current status: "+status)
+            time.sleep(30)
 
-    is_complete=run_command(status_command)[0].find('Complete')
+        is_complete=run_command(status_command)[0].find('Complete')
 
-    if is_complete < 0:
-      bs.put(json.dumps(job_details))
-      debug_log("Build is not successful putting it to failed build tube")
+        //checking logs for the build phase
+        log_command = "oc logs --namespace "+name+"-"+tag+" build/"+build_details+" --config="+config_path+"/node.kubeconfig"
+        logs = run_command(log_command)
 
-    return 0
-  except Exception as e:
-    logger.log(level=logging.CRITICAL, msg=e.message)
-    return 1
+        if is_complete < 0:
+            bs.put(json.dumps(job_details))
+            notify_build_failure(name+"-"+tag, notify_email, logs);
+            debug_log("Build is not successful putting it to failed build tube")
+
+        return 0
+    except Exception as e:
+        logger.log(level=logging.CRITICAL, msg=e.message)
+        return 1
 
 while True:
-  try:
-    debug_log("listening to start_build tube")
-    job = bs.reserve()
-    job_details = json.loads(job.body) 
-    result = start_build(job_details)
-    if result == 0:
-      debug_log("Build is successful deleting the job")
-      job.delete()
-    else:
-      debug_log("Job was not succesfull and returned to tube")
-  except Exception as e:
-    logger.log(level=logging.CRITICAL, msg=e.message)
+    try:
+        debug_log("listening to start_build tube")
+        job = bs.reserve()
+        job_details = json.loads(job.body)
+        result = start_build(job_details)
+        if result == 0:
+            debug_log("Build is successful deleting the job")
+            job.delete()
+        else:
+            debug_log("Job was not succesfull and returned to tube")
+    except Exception as e:
+        logger.log(level=logging.CRITICAL, msg=e.message)
