1dbdd95de9412d374a0964e62ffd5a2014f9c0ec
Karanbir Singh <kbsingh@karan.org>
2016-10-09 01:38:22 +0100

    Clearly state the build status via a Fail/Success header and clean up the run-delivery script

--- server/run-delivery.sh
+++ server/run-delivery.sh
@@ -9,24 +9,13 @@ if [ "${TARGET_REGISTRY}" == "" ];then
     TARGET_REGISTRY=${OUTPUT_REGISTRY}
 fi
 
-#_ "Log in to the internal registry"
-#docker login -u serviceaccount -p ${TOKEN} -e serviceaccount@example.org ${OUTPUT_REGISTRY}
-
 FULL_FROM=${TARGET_REGISTRY}/${TARGET_NAMESPACE}/${FROM}
 FULL_TO=${TARGET_REGISTRY}/${TARGET_NAMESPACE}/${TO}
 
 _ "Pulling RC image (${FROM})"
 docker pull ${FULL_FROM}
 
-#_ "Running delivery steps"
-#docker run --rm ${FULL_FROM} --entrypoint /bin/bash /usr/bin/delivery_script
-
 sleep 20
-
-#_ "Starting mail server"
-#export REPLYTO=container-build@centos.org
-#mkfifo /var/spool/postfix/public/pickup
-#postfix start
 
 _ "Tagging for the public registry"
 docker tag ${FULL_FROM} ${FULL_TO}
@@ -35,11 +24,6 @@ _ "Pushing final image (${FULL_TO})"
 docker push ${FULL_TO}
 
 OUTPUT_IMAGE=registry.centos.org/${TARGET_NAMESPACE}/${TO}
-
-#_ "Send mail to (${NOTIFY_EMAIL}) notify build is completed (${OUTPUT_IMAGE})"
-#echo "Build is successful please pull the image (${OUTPUT_IMAGE})" | mail -r container-build-report@centos.org -s "cccp-build is complete" ${NOTIFY_EMAIL}
-
-#sleep 20
 
 _ "Send success mail for user notify tube"
 python /tube_request/send_notify_request.py ${BEANSTALK_SERVER} ${OUTPUT_IMAGE} ${NOTIFY_EMAIL}
