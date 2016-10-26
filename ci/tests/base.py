566524144b8f5577bf45fc5fa34d437a25878d65
Ratnadeep Debnath <rtnpro@gmail.com>
2016-10-26 11:29:11 +0530

    Added README for writing tests and update docs.

--- ci/tests/base.py
+++ ci/tests/base.py
@@ -6,6 +6,7 @@ from ci.lib import provision, run_cmd
 
 
 class BaseTestCase(unittest.TestCase):
+    """Base test case to extend test cases from"""
 
     def setUp(self):
         self.hosts = json.loads(os.environ.get('CCCP_CI_HOSTS') or "{}") or {
@@ -35,9 +36,33 @@ class BaseTestCase(unittest.TestCase):
         }
 
     def provision(self, force=False):
+        """
+        Provision CCCP nodes.
+
+        By default, it runs provisioning only for the first time, and
+        skips for the subsequent calls.
+
+        Args:
+            force (bool): Provision forcefully.
+        """
         provision(self.hosts['controller'], force=force)
 
     def run_cmd(self, cmd, user=None, host=None, stream=False):
+        """
+        Run command on local or remote machine (over SSH).
+
+        Args:
+            cmd (str): Command to execute
+            user (str): Remote user to execute command as
+            host (str): Remote host
+            stream (bool): Whether to stream output or not
+
+        Returns:
+            Output string
+
+        Raises:
+            Exception if command execution fails
+        """
         host_info = self.hosts.get(self.node)
         return run_cmd(cmd, user=user or host_info['remote_user'],
                        host=host or host_info['host'],
