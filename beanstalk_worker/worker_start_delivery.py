6c5b7f692c3a0a2e0725bad446ca399bd09a068a
Bamacharan Kundu <bamachrn@gmail.com>
2016-11-21 09:42:07 +0530

    OpenShift Server IP replaced with variable

--- beanstalk_worker/worker_start_delivery.py
+++ beanstalk_worker/worker_start_delivery.py
@@ -27,24 +27,29 @@ logger.addHandler(ch)
 
 config_path = "/".join(sys.argv[0].split("/")[:-1])
 
-DEBUG=1
+DEBUG = 1
+
 
 def notify_build_failure(name_space, notify_email, logs):
     msg_details = {}
     msg_details['action'] = 'notify_user'
-    msg_details['subject'] = "FAILED: Container-build failed"+name_space
-    msg_details['msg'] = "Container build "+ name_space +" failed due to error in build or test steps. Pleae check attached logs"
+    msg_details['subject'] = "FAILED: Container-build failed" + name_space
+    msg_details['msg'] = "Container build " + name_space + \
+        " failed due to error in build or test steps. Pleae check attached logs"
     msg_details['logs'] = logs
     msg_details['notify_email'] = notify_email
     bs.use('master_tube')
     bs.put(json.dumps(msg_details))
 
+
 def debug_log(msg):
-    if DEBUG==1:
-        logger.log(level=logging.INFO,msg=msg)
+    if DEBUG == 1:
+        logger.log(level=logging.INFO, msg=msg)
+
 
 def run_command(command):
-    p = Popen(command,bufsize=0,shell = True,stdout = PIPE,stderr = PIPE,stdin = PIPE)
+    p = Popen(command, bufsize=0, shell=True,
+              stdout=PIPE, stderr=PIPE, stdin=PIPE)
     p.wait()
     out = p.communicate()
     return out
@@ -59,50 +64,57 @@ def start_delivery(job_details):
         #tag = job_details['tag']
         #depends_on = job_details['depends_on']
 
-        debug_log("Login to openshift server")
-        command_login = "oc login https://openshift:8443 -u test-admin -p test --config="+config_path+"/node.kubeconfig --certificate-authority="+config_path+"/ca.crt"
+        debug_log("Login to OpenShift server")
+        command_login = "oc login https://OPENSHIFT_SERVER_IP:8443 -u test-admin -p test --config=" + \
+            config_path + "/node.kubeconfig --certificate-authority=" + config_path + "/ca.crt"
         out = run_command(command_login)
         debug_log(out)
 
         debug_log(" change project to the desired one")
-        command_change_project = "oc project "+name_space+" --config="+config_path+"/node.kubeconfig"
+        command_change_project = "oc project " + name_space + \
+            " --config=" + config_path + "/node.kubeconfig"
         out = run_command(command_change_project)
         debug_log(out)
 
         debug_log("start the delivery")
-        command_start_build = "oc --namespace "+name_space+" start-build delivery --config="+config_path+"/node.kubeconfig"
+        command_start_build = "oc --namespace " + name_space + \
+            " start-build delivery --config=" + config_path + "/node.kubeconfig"
         out = run_command(command_start_build)
         debug_log(out)
 
         build_details = out[0].rstrip()
         debug_log(build_details)
 
-        if build_details=="":
-            logger.log(level=logging.CRITICAL, msg="build could not be started as openshift is not reachable")
+        if build_details == "":
+            logger.log(level=logging.CRITICAL,
+                       msg="build could not be started as OpenShift is not reachable")
             return 1
 
-        debug_log("Delivery started is "+build_details)
+        debug_log("Delivery started is " + build_details)
 
-        status_command = "oc get --namespace "+name_space+" build/"+build_details+" --config="+config_path+"/node.kubeconfig|grep -v STATUS"
+        status_command = "oc get --namespace " + name_space + " build/" + \
+            build_details + " --config=" + config_path + "/node.kubeconfig|grep -v STATUS"
         is_running = 1
 
         debug_log("Checking the delivery status")
         while is_running >= 0:
-            status= run_command(status_command)[0].rstrip()
-            is_running = re.search("New|Pending|Running",status)
-            debug_log("current status: "+status)
+            status = run_command(status_command)[0].rstrip()
+            is_running = re.search("New|Pending|Running", status)
+            debug_log("current status: " + status)
             time.sleep(30)
 
-        is_complete=run_command(status_command)[0].find('Complete')
-        #checking logs for the build phase
-        log_command = "oc logs --namespace "+name_space+" build/"+build_details+" --config="+config_path+"/node.kubeconfig"
+        is_complete = run_command(status_command)[0].find('Complete')
+        # checking logs for the build phase
+        log_command = "oc logs --namespace " + name_space + " build/" + \
+            build_details + " --config=" + config_path + "/node.kubeconfig"
         logs = run_command(log_command)
         logs = logs[0]
 
         if is_complete < 0:
             bs.put(json.dumps(job_details))
             notify_build_failure(name_space, notify_email, logs)
-            debug_log("Delivery is not successful putting it to failed build tube")
+            debug_log(
+                "Delivery is not successful putting it to failed build tube")
 
         return 0
     except Exception as e:
