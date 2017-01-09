fbc6b4855dda0967a4f98b59b746009e7a0cdc4a
Navid Shaikh <nshaikh@redhat.com>
2017-01-09 15:42:16 +0530

    Adds scanner output result filenames in constants

--- beanstalk_worker/constants.py
+++ beanstalk_worker/constants.py
@@ -6,3 +6,13 @@ SCANNERS_OUTPUT = {
         "registry.centos.org/pipeline-images/scanner-rpm-verify": [
             "RPMVerify.json"]
 }
+
+SCANNER_LOGS_NFS = {
+        "registry.centos.org/pipeline-images/pipeline-scanner": [
+            "pipeline_scanner_results.json"],
+        "registry.centos.org/pipeline-images/misc-package-updates": [
+            "misc_package_updates_scanner_results.json"],
+        "registry.centos.org/pipeline-images/scanner-rpm-verify": [
+            "RPMVerify_scanner_results.json"]
+}
+
