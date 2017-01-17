6afaa3bb5e0e3739eaf8899c30396c9e4708589a
mohammedzee1000 <mohammed.zee1000@gmail.com>
2017-01-17 12:47:49 +0530

    Fixed issue in regex pattern match for depends on.
    Dots included after : were taken as mismatch and this has been
    fixed.

--- testindex/validators.py
+++ testindex/validators.py
@@ -189,7 +189,7 @@ class IndexFormatValidator(IndexValidator):
                 if not isinstance(depends_on, list):
                     depends_on = [depends_on]
                 matcher = re.compile("^(([0-9a-zA-Z_-]+[.]{1})*([0-9a-zA-Z_-]+){1}[/]{1})?[0-9a-zA-Z_-]+[/]{1}"
-                                     "[0-9a-zA-Z_-]+[:]{1}[0-9a-zA-Z_-]+$")
+                                     "[0-9a-zA-Z_-]+[:]{1}([0-9a-zA-Z_-]+\.?)+$")
                 for item in depends_on:
                     if not matcher.search(str(item)):
                         self._mark_entry_invalid(entry)
