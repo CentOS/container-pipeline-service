c487ac215ec07a098537b07c50aa03e5935dd795
Ratnadeep Debnath <rtnpro@gmail.com>
2017-03-28 14:05:11 +0530

    Moved monitoring scripts to inside tracking dir.
    Updated repo upstreams for centos bse image.

---
+++ src/tracking/config.py
@@ -0,0 +1,12 @@
+upstreams = {
+    'centos7': {
+        'baseurls': [
+            'http://mirrorlist.centos.org/?release=7&arch=x86_64&repo=os&infra=container',
+            'http://mirrorlist.centos.org/?release=7&arch=x86_64&repo=extras&infra=container',
+            'http://mirrorlist.centos.org/?release=7&arch=x86_64&repo=updates&infra=container'
+        ],
+        'arch': 'x86_64'
+    }
+}
+
+datadir = "./"
