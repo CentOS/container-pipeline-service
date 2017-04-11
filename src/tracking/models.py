0ef2e70cff471c3667662295221868339916f1cd
Ratnadeep Debnath <rtnpro@gmail.com>
2017-04-11 09:57:28 +0530

    Scan container image to retrieve enabled repos
    along with some other changes
    - renamed command from populateimagepkgs to imagescanner
    - Added new fields to ContainerImage model: scanned (bool), last_scanned (datetime)

--- src/tracking/models.py
+++ src/tracking/models.py
@@ -24,6 +24,20 @@ class Package(models.Model):
                                     self.arch)
 
 
+class RepoInfo(models.Model):
+    baseurls = models.TextField(max_length=2000, db_index=True,
+                                help_text="Repo urls", unique=True)
+    basearch = models.CharField(max_length=20, db_index=True)
+    releasever = models.CharField(max_length=10, db_index=True)
+    infra = models.CharField(max_length=50)
+
+    class Meta:
+        db_table = 'repo_info'
+
+    def __str__(self):
+        return self.baseurls
+
+
 class ContainerImage(models.Model):
     name = models.CharField(max_length=200, db_index=True,
                             help_text="Image name", unique=True)
@@ -31,8 +45,13 @@ class ContainerImage(models.Model):
                                       help_text="Packages")
     parents = models.ManyToManyField('self', symmetrical=False,
                                      help_text="Parent images")
+    repoinfo = models.ForeignKey(RepoInfo, null=True)
+
     to_build = models.BooleanField(default=False, db_index=True,
                                    help_text='Whether to build image or not')
+    scanned = models.BooleanField(default=False, db_index=True,
+                                  help_text='Whether the image is scanner')
+    last_scanned = models.DateTimeField(null=True, blank=True)
 
     created = models.DateTimeField(auto_now_add=True, blank=True)
     last_updated = models.DateTimeField(auto_now=True, blank=True)
