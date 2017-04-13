57ed4cdfde2d697c71ed1e486a7067f45eb420d5
Ratnadeep Debnath <rtnpro@gmail.com>
2017-04-13 20:25:47 +0530

    Make triggerbuilds service fault tolerant.

--- src/tracking/management/commands/triggerbuilds.py
+++ src/tracking/management/commands/triggerbuilds.py
@@ -18,36 +18,46 @@ class Command(BaseCommand):
             'Triggering image builds...')
         while True:
             try:
-                last_updated = Package.objects.all().order_by(
-                    '-last_updated')[0].last_updated
-            except IndexError:
-                continue
-            if (timezone.now() - last_updated).seconds < (
-                    settings.CONTAINER_BUILD_TRIGGER_DELAY):
-                logger.info('Waiting for package change burst to complete')
-                time.sleep(settings.CONTAINER_BUILD_TRIGGER_DELAY)
-                continue
-            to_build = set()
-            qs = ContainerImage.objects.filter(to_build=True)
-            for container in qs:
-                parents_in_to_build = qs.filter(
-                    id__in=[item.id for item in container.parents.all()])
-                if parents_in_to_build:
+                try:
+                    last_updated = Package.objects.all().order_by(
+                        '-last_updated')[0].last_updated
+                except IndexError:
+                    continue
+                if (timezone.now() - last_updated).seconds < (
+                        settings.CONTAINER_BUILD_TRIGGER_DELAY):
+                    logger.info('Waiting for package change burst to complete')
+                    time.sleep(settings.CONTAINER_BUILD_TRIGGER_DELAY)
+                    continue
+                to_build = set()
+                qs = ContainerImage.objects.filter(to_build=True)
+                for container in qs:
+                    parents_in_to_build = qs.filter(
+                        id__in=[item.id for item in container.parents.all()])
+                    if parents_in_to_build:
+                        try:
+                            to_build.remove(container)
+                        except KeyError:
+                            pass
+                        container.to_build = False
+                        container.save()
+                        logger.debug('Removed image: %s from build queue'
+                                     % container)
+                        logger.debug('Parents in build queue: %s' %
+                                     parents_in_to_build)
+                    else:
+                        to_build.add(container)
+                for c in to_build:
                     try:
-                        to_build.remove(container)
-                    except KeyError:
-                        pass
-                    container.to_build = False
-                    container.save()
-                    logger.debug('Removed image: %s from build queue'
-                                 % container)
-                    logger.debug('Parents in build queue: %s' %
-                                 parents_in_to_build)
-                else:
-                    to_build.add(container)
-            for c in to_build:
-                logger.info('Triggering image build for %s' % c)
-                c.trigger_build()
-                c.to_build = False
-                c.save()
-            time.sleep(settings.CONTAINER_BUILD_TRIGGER_DELAY)
+                        logger.info('Triggering image build for %s' % c)
+                        c.trigger_build()
+                        c.to_build = False
+                        c.save()
+                    except Exception as e:
+                        logger.error(
+                            'Error during triggering build for image %s: %s'
+                            % (c, e), exc_info=True)
+            except Exception as e:
+                logger.error(
+                     'Error in trigger build loop: %s' % e, exc_info=True)
+            finally:
+                time.sleep(settings.CONTAINER_BUILD_TRIGGER_DELAY)
