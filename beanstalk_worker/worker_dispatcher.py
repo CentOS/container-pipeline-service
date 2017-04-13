4f3ac380ba3204dd556713fbb3ae5d30010a015f
Navid Shaikh <nshaikh@redhat.com>
2017-04-13 18:13:15 +0530

    Adds missing centralize logging for dispatch worker
      Enroutes all the messages generated from dispatch worker to
      centralize logging. It is deployed as a container.

--- beanstalk_worker/worker_dispatcher.py
+++ beanstalk_worker/worker_dispatcher.py
@@ -1,46 +1,56 @@
 #!/usr/bin/env python
 
-import beanstalkc
 import json
-import os
-import re
-import time
+import logging
+
+
+import beanstalkc
+import config
+
+
+config.load_logger()
+logger = logging.getLogger("dispatcher")
 
 bs = beanstalkc.Connection(host="BEANSTALK_SERVER")
 bs.watch("master_tube")
 
 while True:
-  print "listening to master_tube"
-  job = bs.reserve()
-  job_details = json.loads(job.body)
+    logger.debug("Listening to master_tube")
+    job = bs.reserve()
+    job_details = json.loads(job.body)
 
-  print "==> Retrieving job action"
-  action = job_details['action']
+    logger.info("==> Retrieving job action")
+    action = job_details['action']
 
-  if action == "start_build":
-    bs.use("start_build")
-    bs.put(json.dumps(job_details))
-    print "==> Job moved to build tube"
-  elif action == "start_scan":
-    bs.use("start_scan")
-    bs.put(json.dumps(job_details))
-    print "==> Job moved to test tube"
-  elif action == "start_delivery":
-    bs.use("start_delivery")
-    bs.put(json.dumps(job_details))
-    print "==> Job moved to delivery tube"
-  elif action == "notify_user":
-    bs.use("notify_user")
-    bs.put(json.dumps(job_details))
-    print "==> Job moved to notify tube"
-  elif action == "report_scan_results":
-    bs.use("report_scan_results")
-    bs.put(json.dumps(job_details))
-    print "==> Job moved to report scan results tube"
-  elif action == "start_linter":
-    bs.use("start_linter")
-    bs.put(json.dumps(job_details))
-    print "==> Job moved to linter tube"
+    if action == "start_build":
+        bs.use("start_build")
+        bs.put(json.dumps(job_details))
+        logger.info("==> Job moved to build tube")
 
-  print "==> Deleting job"
-  job.delete()
+    elif action == "start_scan":
+        bs.use("start_scan")
+        bs.put(json.dumps(job_details))
+        logger.info("==> Job moved to test tube")
+
+    elif action == "start_delivery":
+        bs.use("start_delivery")
+        bs.put(json.dumps(job_details))
+        logger.info("==> Job moved to delivery tube")
+
+    elif action == "notify_user":
+        bs.use("notify_user")
+        bs.put(json.dumps(job_details))
+        logger.info("==> Job moved to notify tube")
+
+    elif action == "report_scan_results":
+        bs.use("report_scan_results")
+        bs.put(json.dumps(job_details))
+        logger.info("==> Job moved to report scan results tube")
+
+    elif action == "start_linter":
+        bs.use("start_linter")
+        bs.put(json.dumps(job_details))
+        logger.info("==> Job moved to linter tube")
+
+    logger.debug("==> Deleting job: %s" % job_details)
+    job.delete()
