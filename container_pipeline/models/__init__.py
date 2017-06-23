0368f7763e990ebc7979dc859d7616128e470a7b
Ratnadeep Debnath <rtnpro@gmail.com>
2017-06-23 14:39:23 +0530

    Added models for project, build, build phase.

--- container_pipeline/models/__init__.py
+++ container_pipeline/models/__init__.py
@@ -1,1 +1,2 @@
 from tracking import *  # noqa
+from pipeline import *  # noqa
---
+++ container_pipeline/models/pipeline.py
@@ -0,0 +1,93 @@
+from django.db import models
+
+
+class Project(models.Model):
+    name = models.CharField(max_length=200, unique=True, db_index=True)
+    uuid = models.CharField(max_length=100, unique=True, db_index=True)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        app_label = 'container_pipeline'
+        db_table = 'projects'
+
+    def __str__(self):
+        return self.name
+
+
+class Build(models.Model):
+    uuid = models.CharField(max_length=100, unique=True, db_index=True)
+    project = models.ForeignKey(Project)
+
+    STATUS_CHOICES = (
+        ('queued', 'Queued'),
+        ('processing', 'In process'),
+        ('complete', 'Complete'),
+        ('error', 'Error'),
+        ('failed', 'Failed')
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,
+                              db_index=True, blank=True)
+
+    logs = models.CharField(max_length=200, null=True)
+
+    NOTIFICATION_STATUS_CHOICES = (
+        ('queued', 'Queued'),
+        ('sent', 'Sent'),
+        ('failed', 'Failed')
+    )
+    notification_status = models.CharField(
+        max_length=20, choices=NOTIFICATION_STATUS_CHOICES, db_index=True,
+        default='', blank=True)
+
+    start_time = models.DateTimeField(default=None, blank=True)
+    end_time = models.DateTimeField(default=None, blank=True)
+
+    trigger = models.CharField(max_length=20, default=None, blank=True)
+    trigger_details = models.TextField(max_length=100, default=None,
+                                       blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        app_label = 'container_pipeline'
+        db_table = 'builds'
+
+    def __str__(self):
+        return '{}:{}'.format(self.project, self.uuid)
+
+
+class BuildPhase(models.Model):
+    build = models.ForeignKey(Build)
+    PHASE_CHOICES = (
+        ('build', 'Build'),
+        ('test', 'Test'),
+        ('scan', 'Scan'),
+        ('delivery', 'Delivery')
+    )
+    phase = models.CharField(max_length=20, choices=PHASE_CHOICES,
+                             db_index=True)
+    STATUS_CHOICES = (
+        ('queued', 'Queued'),
+        ('processing', 'In process'),
+        ('complete', 'Complete'),
+        ('error', 'Error'),
+        ('failed', 'Failed')
+    )
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES,
+                              db_index=True, blank=True)
+
+    start_time = models.DateTimeField(default=None, blank=True)
+    end_time = models.DateTimeField(default=None, blank=True)
+
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        app_label = 'container_pipeline'
+        db_table = 'build_phases'
+
+    def __str__(self):
+        return '{}:{}'.format(self.build, self.phase)
