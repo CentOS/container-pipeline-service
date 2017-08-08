0138f8ec966149b1242860a8ca69ab8d87cb4c4b
Dharmit Shah <shahdharmit@gmail.com>
2017-08-08 11:59:21 +0530

    Generate project_name variable before triggering dockerfile linter

--- container_pipeline/trigger_dockerfile_lint.py
+++ container_pipeline/trigger_dockerfile_lint.py
@@ -5,6 +5,7 @@ import os
 
 from container_pipeline.lib import settings
 from container_pipeline.lib.queue import JobQueue
+from container_pipeline.utils import get_project_name
 
 import utils
 
@@ -30,13 +31,30 @@ def trigger_dockerfile_linter(appid, jobid, repo_url, repo_branch,
     else:
         filename = target_file
 
-    lint_job_data = {}
+    lint_job_data = {
+        "appid": appid,
+        "job_name": job_name,
+        "notify_email": notify_email,
+        "logs_dir": logs_dir,
+        "action": "start_linter",
+        "jobid": jobid,
+        "repo_url": repo_url,
+        "repo_branch": repo_branch,
+        "repo_build_path": repo_build_path,
+        "target_file": target_file,
+        "desired_tag": desired_tag,
+        "depends_on": depends_on,
+        "test_tag": test_tag
+    }
+
+    lint_job_data["project_name"] = get_project_name(lint_job_data)
 
     try:
         dockerfile_location = \
             os.path.join(os.environ['DOCKERFILE_DIR'], git_path, filename)
         with open(dockerfile_location) as f:
             dockerfile = f.read()
+        lint_job_data["dockerfile"] = dockerfile
     except IOError as e:
         print "==> Error opening the file %s" % dockerfile_location
         print "==> Error: %s" % str(e)
@@ -56,22 +74,5 @@ def trigger_dockerfile_linter(appid, jobid, repo_url, repo_branch,
         print "==>Put job on 'master_tube' tube"
         return False
     else:
-        lint_job_data = {
-            "appid": appid,
-            "job_name": job_name,
-            "dockerfile": dockerfile,
-            "notify_email": notify_email,
-            "logs_dir": logs_dir,
-            "action": "start_linter",
-            "jobid": jobid,
-            "repo_url": repo_url,
-            "repo_branch": repo_branch,
-            "repo_build_path": repo_build_path,
-            "target_file": target_file,
-            "desired_tag": desired_tag,
-            "depends_on": depends_on,
-            "test_tag": test_tag
-        }
-
         queue.put(json.dumps(lint_job_data), tube="master_tube")
         return True
