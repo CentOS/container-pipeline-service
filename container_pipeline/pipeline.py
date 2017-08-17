d527206877c74bfed88540c5d43df186b48b1791
Dharmit Shah <shahdharmit@gmail.com>
2017-08-17 18:00:31 +0530

    Move job creation to pipeline.py file

--- container_pipeline/pipeline.py
+++ container_pipeline/pipeline.py
@@ -3,60 +3,128 @@ import os
 import sys
 import time
 
-import container_pipeline.utils as utils
 from container_pipeline.lib.log import load_logger
 from container_pipeline.lib.openshift import Openshift, OpenshiftError
 from trigger_dockerfile_lint import trigger_dockerfile_linter
+from container_pipeline.utils import get_project_name, get_job_hash
 
 
-def create_project(appid, jobid, repo_url, repo_branch, repo_build_path,
-                   target_file, notify_email, desired_tag, depends_on,
-                   test_tag):
-    job_name = utils.get_job_name({
-        'appid': appid, 'jobid': jobid, 'desired_tag': desired_tag})
-    project = utils.get_job_hash(job_name)
+def create_new_job():
+    """
+    Creates new job (a dictionary) with default value as `None` for all the
+    keys. This is the central place for creation of a job for container
+    pipeline service
+    """
+    job = dict.fromkeys([
+        "project_name",  # centos/centos:latest will be centos-centos-latest
+        "notify_email",  # email to send notifications to
+        "depends_on",    # parent image. Rebuild of parent will cause child to
+                         # rebuild as well
+        "repo_branch",   # branch of the repository to checkout
+        "logs_dir",      # directory where all workers' logs files will be
+                         # stored
+        "test_tag",      # temporary tag to be applied to image
+        "repo_url",      # url of the remote git repository to build
+        "repo_build_path",  # path on the repo where Dockerfile can be found
+        "target_file",   # Name of the Dockerfile to use to build container
+                         # image
+        "jobid",         # equivalent to image name in Docker hub lingo
+        "desired_tag",   # tag to be applied to the image
+        "appid",         # equivalent to namespacein Docker hub lingo
+        "action",        # action to be performed (lint, build, scan, etc.)
+        "dockerfile",    # contents of the Dockerfile for linter purpose
+        "job_name",      # same as project_name. need to remove
+        "output_image",  # full path to the built image
+        "namespace",     # again same as project_name & job_name. need to
+                         # remove
+        "image_name",    # <appid>/<jobid>:<desired_tag>
+        "logs_URL",      # https URL for the logs hosted on nginx
+        "logs_file_path",   # path to the logs of all scanners
+        "build_status",  # status of build process
+        "lint_status",   # status of lint process
+        "scan_status",   # status of scan process
+        "delivery_status",   # status of delivery process
+        "job_hash_key"   # hash value of `project_name` key
+    ])
+
+    return job
+
+
+def create_project(job):
+    # job_name = utils.get_job_name({
+    #     'appid': appid, 'jobid': jobid, 'desired_tag': desired_tag})
+    # project = utils.get_job_hash(job_name)
     openshift = Openshift(logger=logger)
     try:
         openshift.login("test-admin", "test")
-        if openshift.get_project(project):
-            openshift.delete(project)
+        if openshift.get_project(job["project_name"]):
+            openshift.delete(job["project_name"])
             time.sleep(50)
-        openshift.create(project)
+        openshift.create(job["project_name"])
     except OpenshiftError:
         try:
-            openshift.delete(project)
+            openshift.delete(job["project_name"])
         except OpenshiftError as e:
             logger.error(e)
         raise
 
     template_path = os.path.join(
         os.path.dirname(__file__), 'template.json')
-    openshift.upload_template(project, template_path, {
-        'SOURCE_REPOSITORY_URL': repo_url,
-        'REPO_BRANCH': repo_branch,
-        'APPID': appid,
-        'JOBID': jobid,
-        'REPO_BUILD_PATH': repo_build_path,
-        'TARGET_FILE': target_file,
-        'NOTIFY_EMAIL': notify_email,
-        'DESIRED_TAG': desired_tag,
-        'TEST_TAG': test_tag})
+    openshift.upload_template(job["project_name"], template_path, {
+        'SOURCE_REPOSITORY_URL': job["repo_url"],
+        'REPO_BRANCH': job["repo_branch"],
+        'APPID': job["appid"],
+        'JOBID': job["jobid"],
+        'REPO_BUILD_PATH': job["repo_build_path"],
+        'TARGET_FILE': job["target_file"],
+        'NOTIFY_EMAIL': job["notify_email"],
+        'DESIRED_TAG': job["desired_tag"],
+        'TEST_TAG': job["test_tag"]
+    })
+
+
+def main(args):
+    # create new job
+    job = create_new_job()
+
+    (appid, jobid, repo_url, repo_branch, repo_build_path,
+     target_file, notify_email, desired_tag, depends_on,
+     test_tag) = args
+
+    # having '/' in a value used in os.path.join generates unexpected paths
+    if repo_build_path.startswith("/"):
+        repo_build_path = repo_build_path[1:]
+
+    if target_file.startswith("/"):
+        target_file = target_file[1:]
+
+    # populate job's keys with appropriate values
+    job["appid"] = appid
+    job["notify_email"] = notify_email
+    job["logs_dir"] = '/srv/pipeline-logs/{}'.format(test_tag)
+    job["action"] = "start_linter"
+    job["jobid"] = jobid
+    job["repo_url"] = repo_url
+    job["repo_branch"] = repo_branch
+    job["repo_build_path"] = repo_build_path
+    job["target_file"] = target_file
+    job["desired_tag"] = desired_tag
+    job["depends_on"] = depends_on
+    job["test_tag"] = test_tag
+
+    job["project_name"] = get_project_name(job)
+    job["job_hash_key"] = get_job_hash(job["project_name"])
+    job["job_name"] = job["project_name"]
+
+    try:
+        linter_status = trigger_dockerfile_linter(job)
+        if linter_status:
+            create_project(job)
+    except OpenshiftError:
+        sys.exit(1)
 
 
 if __name__ == '__main__':
     load_logger()
     logger = logging.getLogger('jenkins')
-    (appid, jobid, repo_url, repo_branch, repo_build_path,
-     target_file, notify_email, desired_tag, depends_on,
-     test_tag) = sys.argv[1:]
-    try:
-        linter_status = trigger_dockerfile_linter(
-            appid, jobid, repo_url, repo_branch,
-            repo_build_path, target_file, notify_email,
-            desired_tag, depends_on, test_tag)
-        if linter_status:
-            create_project(appid, jobid, repo_url, repo_branch, repo_build_path,
-                           target_file, notify_email, desired_tag, depends_on,
-                           test_tag)
-    except OpenshiftError:
-        sys.exit(1)
+    main(sys.argv[1:])
--- container_pipeline/trigger_dockerfile_lint.py
+++ container_pipeline/trigger_dockerfile_lint.py
@@ -5,97 +5,18 @@ import os
 
 from container_pipeline.lib import settings
 from container_pipeline.lib.queue import JobQueue
-from container_pipeline.utils import get_project_name, get_job_hash
-
-import utils
 
 
-def create_new_job():
-    """
-    Creates new job (a dictionary) with default value as `None` for all the
-    keys. This is the central place for creation of a job for container
-    pipeline service
-    """
-    job = dict.fromkeys([
-        "project_name",  # centos/centos:latest will be centos-centos-latest
-        "notify_email",  # email to send notifications to
-        "depends_on",    # parent image. Rebuild of parent will cause child to
-                         # rebuild as well
-        "repo_branch",   # branch of the repository to checkout
-        "logs_dir",      # directory where all workers' logs files will be
-                         # stored
-        "test_tag",      # temporary tag to be applied to image
-        "repo_url",      # url of the remote git repository to build
-        "repo_build_path",  # path on the repo where Dockerfile can be found
-        "target_file",   # Name of the Dockerfile to use to build container
-                         # image
-        "jobid",         # equivalent to image name in Docker hub lingo
-        "desired_tag",   # tag to be applied to the image
-        "appid",         # equivalent to namespacein Docker hub lingo
-        "action",        # action to be performed (lint, build, scan, etc.)
-        "dockerfile",    # contents of the Dockerfile for linter purpose
-        "job_name",      # same as project_name. need to remove
-        "output_image",  # full path to the built image
-        "namespace",     # again same as project_name & job_name. need to
-                         # remove
-        "image_name",    # <appid>/<jobid>:<desired_tag>
-        "logs_URL",      # https URL for the logs hosted on nginx
-        "logs_file_path",   # path to the logs of all scanners
-        "build_status",  # status of build process
-        "lint_status",   # status of lint process
-        "scan_status",   # status of scan process
-        "delivery_status",   # status of delivery process
-        "job_hash_key"   # hash value of `project_name` key
-    ])
-
-    return job
-
-
-def trigger_dockerfile_linter(appid, jobid, repo_url, repo_branch,
-                              repo_build_path, target_file, notify_email,
-                              desired_tag, depends_on, test_tag):
+def trigger_dockerfile_linter(job):
     queue = JobQueue(
         host=settings.BEANSTALKD_HOST,
         port=settings.BEANSTALKD_PORT,
         sub="master_tube")
 
-    job_name = utils.get_job_name({
-        'appid': appid, 'jobid': jobid, 'desired_tag': desired_tag})
-    logs_dir = '/srv/pipeline-logs/{}'.format(test_tag)
-
-    # having '/' in a value used in os.path.join generates unexpected paths
-    if repo_build_path.startswith("/"):
-        git_path = repo_build_path[1:]
-
-    if target_file.startswith("/"):
-        filename = target_file[1:]
-    else:
-        filename = target_file
-
-    # create new job
-    job = create_new_job()
-
-    # populate job's keys with appropriate values
-    job["appid"] = appid
-    job["job_name"] = job_name
-    job["notify_email"] = notify_email
-    job["logs_dir"] = logs_dir
-    job["action"] = "start_linter"
-    job["jobid"] = jobid
-    job["repo_url"] = repo_url
-    job["repo_branch"] = repo_branch
-    job["repo_build_path"] = repo_build_path
-    job["target_file"] = target_file
-    job["desired_tag"] = desired_tag
-    job["depends_on"] = depends_on
-    job["test_tag"] = test_tag
-
-    job["project_name"] = get_project_name(job)
-    job["job_hash_key"] = get_job_hash(job["project_name"])
-
     try:
         dockerfile_location = \
-            os.path.join(os.environ['DOCKERFILE_DIR'], git_path, filename)
+            os.path.join(os.environ['DOCKERFILE_DIR'], job["repo_build_path"],
+                         job["target_file"])
         with open(dockerfile_location) as f:
             dockerfile = f.read()
         job["dockerfile"] = dockerfile
@@ -105,15 +26,15 @@ def trigger_dockerfile_linter(appid, jobid, repo_url, repo_branch,
         print "==> Sending Dockerfile linter failure email"
         response = {
             "action": "notify_user",
-            "namespace": appid,
-            "notify_email": notify_email,
-            "job_name": job_name,
+            "namespace": job["appid"],
+            "notify_email": job["notify_email"],
+            "job_name": job["job_name"],
             "msg": "Couldn't find the Dockerfile at specified git_path",
-            "logs_dir": logs_dir,
+            "logs_dir": job["logs_dir"],
             "lint_status": False,
             "build_status": False,
-            "project_name": get_project_name(job),
-            "test_tag": test_tag
+            "project_name": job["project_name"],
+            "test_tag": job["test_tag"]
         }
 
         queue.put(json.dumps(response), tube="master_tube")
@@ -125,15 +46,15 @@ def trigger_dockerfile_linter(appid, jobid, repo_url, repo_branch,
         print "==> Sending Dockerfile linter failure email"
         response = {
             "action": "notify_user",
-            "namespace": appid,
-            "notify_email": notify_email,
-            "job_name": job_name,
+            "namespace": job["appid"],
+            "notify_email": job["notify_email"],
+            "job_name": job["job_name"],
             "msg": "Unexpected error while trigger Dockerfile linter",
-            "logs_dir": logs_dir,
+            "logs_dir": job["logs_dir"],
             "lint_status": False,
             "build_status": False,
-            "project_name": get_project_name(job),
-            "test_tag": test_tag
+            "project_name": job["project_name"],
+            "test_tag": job["test_tag"]
         }
 
         queue.put(json.dumps(response), tube="master_tube")
