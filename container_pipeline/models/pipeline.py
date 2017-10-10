2237400d62b7808f266df6ecdf9a06e645fe72c9
Navid Shaikh <nshaikh@redhat.com>
2017-10-10 17:12:11 +0530

    Adds model of Package for storing data about RPM packages

--- container_pipeline/models/pipeline.py
+++ container_pipeline/models/pipeline.py
@@ -98,3 +98,24 @@ class BuildPhase(models.Model):
 
     def __str__(self):
         return '{}:{}'.format(self.build, self.phase)
+
+
+class Package(models.Model):
+    name = models.CharField(max_length=200, db_index=True,
+                            help_text="Package name")
+    arch = models.CharField(max_length=20, db_index=True,
+                            help_text="Architecture")
+    version = models.CharField(max_length=100, help_text="Version")
+    release = models.CharField(max_length=100, help_text="Release")
+
+    created = models.DateTimeField(auto_now_add=True, blank=True)
+    last_updated = models.DateTimeField(auto_now=True, blank=True)
+
+    class Meta:
+        db_table = 'packages'
+        unique_together = ('name', 'arch', 'version', 'release')
+        app_label = 'container_pipeline'
+
+    def __str__(self):
+        return "{}-{}-{}.{}".format(self.name, self.version, self.release,
+                                    self.arch)
