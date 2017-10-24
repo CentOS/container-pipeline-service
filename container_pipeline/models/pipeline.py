9eb102f5c457f84796823f6d5872fb43cf0fd6a6
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-10-24 11:19:26 +0530

    Commented out common tables between tracking and pipeline.

--- container_pipeline/models/pipeline.py
+++ container_pipeline/models/pipeline.py
@@ -100,63 +100,63 @@ class BuildPhase(models.Model):
         return '{}:{}'.format(self.build, self.phase)
 
 
-class PackageX(models.Model):
-    name = models.CharField(max_length=200, db_index=True,
-                            help_text="Package name")
-    arch = models.CharField(max_length=20, db_index=True,
-                            help_text="Architecture")
-    version = models.CharField(max_length=100, help_text="Version")
-    release = models.CharField(max_length=100, help_text="Release")
-
-    created = models.DateTimeField(auto_now_add=True, blank=True)
-    last_updated = models.DateTimeField(auto_now=True, blank=True)
-
-    class Meta:
-        db_table = 'packages'
-        unique_together = ('name', 'arch', 'version', 'release')
-        app_label = 'container_pipeline'
-
-    def __str__(self):
-        return "{}-{}-{}.{}".format(self.name, self.version, self.release,
-                                    self.arch)
-
-
-class RepoInfoX(models.Model):
-    baseurls = models.TextField(max_length=2000, db_index=True,
-                                help_text="Repo urls", unique=True)
-    basearch = models.CharField(max_length=20, db_index=True)
-    releasever = models.CharField(max_length=10, db_index=True)
-    infra = models.CharField(max_length=50)
-
-    class Meta:
-        db_table = 'repo_info'
-        app_label = 'container_pipeline'
-
-    def __str__(self):
-        return self.baseurls
-
-
-class ContainerImageX(models.Model):
-    name = models.CharField(max_length=200, db_index=True,
-                            help_text="Image name", unique=True)
-    packages = models.ManyToManyField(PackageX, related_name='images',
-                                      help_text="Packages")
-    parents = models.ManyToManyField('self', symmetrical=False,
-                                     help_text="Parent images")
-    repoinfo = models.ForeignKey(RepoInfoX, null=True)
-
-    to_build = models.BooleanField(default=False, db_index=True,
-                                   help_text='Whether to build image or not')
-    scanned = models.BooleanField(default=False, db_index=True,
-                                  help_text='Whether the image is scanner')
-    last_scanned = models.DateTimeField(null=True, blank=True)
-
-    created = models.DateTimeField(auto_now_add=True, blank=True)
-    last_updated = models.DateTimeField(auto_now=True, blank=True)
-
-    class Meta:
-        db_table = 'container_images'
-        app_label = 'container_pipeline'
-
-    def __str__(self):
-        return self.name
+# class PackageX(models.Model):
+#     name = models.CharField(max_length=200, db_index=True,
+#                             help_text="Package name")
+#     arch = models.CharField(max_length=20, db_index=True,
+#                             help_text="Architecture")
+#     version = models.CharField(max_length=100, help_text="Version")
+#     release = models.CharField(max_length=100, help_text="Release")
+#
+#     created = models.DateTimeField(auto_now_add=True, blank=True)
+#     last_updated = models.DateTimeField(auto_now=True, blank=True)
+#
+#     class Meta:
+#         db_table = 'packagesx'
+#         unique_together = ('name', 'arch', 'version', 'release')
+#         app_label = 'container_pipeline'
+#
+#     def __str__(self):
+#         return "{}-{}-{}.{}".format(self.name, self.version, self.release,
+#                                     self.arch)
+#
+#
+# class RepoInfoX(models.Model):
+#     baseurls = models.TextField(max_length=2000, db_index=True,
+#                                 help_text="Repo urls", unique=True)
+#     basearch = models.CharField(max_length=20, db_index=True)
+#     releasever = models.CharField(max_length=10, db_index=True)
+#     infra = models.CharField(max_length=50)
+#
+#     class Meta:
+#         db_table = 'repo_infox'
+#         app_label = 'container_pipeline'
+#
+#     def __str__(self):
+#         return self.baseurls
+#
+#
+# class ContainerImageX(models.Model):
+#     name = models.CharField(max_length=200, db_index=True,
+#                             help_text="Image name", unique=True)
+#     packages = models.ManyToManyField(PackageX, related_name='images',
+#                                       help_text="Packages")
+#     parents = models.ManyToManyField('self', symmetrical=False,
+#                                      help_text="Parent images")
+#     repoinfo = models.ForeignKey(RepoInfoX, null=True)
+#
+#     to_build = models.BooleanField(default=False, db_index=True,
+#                                    help_text='Whether to build image or not')
+#     scanned = models.BooleanField(default=False, db_index=True,
+#                                   help_text='Whether the image is scanner')
+#     last_scanned = models.DateTimeField(null=True, blank=True)
+#
+#     created = models.DateTimeField(auto_now_add=True, blank=True)
+#     last_updated = models.DateTimeField(auto_now=True, blank=True)
+#
+#     class Meta:
+#         db_table = 'container_images'
+#         app_label = 'container_pipeline'
+#
+#     def __str__(self):
+#         return self.name
