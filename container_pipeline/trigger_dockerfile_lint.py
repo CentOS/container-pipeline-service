f152853340155c6469fbd0603906eae181324d9c
Dharmit Shah <shahdharmit@gmail.com>
2017-10-27 12:44:48 +0530

    Use global job when putting data to tube instead of local version of job

--- container_pipeline/trigger_dockerfile_lint.py
+++ container_pipeline/trigger_dockerfile_lint.py
@@ -26,40 +26,48 @@ def trigger_dockerfile_linter(job):
         print "==> Error opening the file %s" % dockerfile_location
         print "==> Error: %s" % str(e)
         print "==> Sending Dockerfile linter failure email"
-        response = {
-            "action": "notify_user",
-            "namespace": job["appid"],
-            "notify_email": job["notify_email"],
-            "job_name": job["job_name"],
-            "msg": "Couldn't find the Dockerfile at specified git_path",
-            "logs_dir": job["logs_dir"],
-            "lint_status": False,
-            "build_status": False,
-            "project_name": job["project_name"],
-            "test_tag": job["test_tag"]
-        }
+        # response = {
+        #     "action": "notify_user",
+        #     "namespace": job["appid"],
+        #     "notify_email": job["notify_email"],
+        #     "job_name": job["job_name"],
+        #     "msg": "Couldn't find the Dockerfile at specified git_path",
+        #     "logs_dir": job["logs_dir"],
+        #     "lint_status": False,
+        #     "build_status": False,
+        #     "project_name": job["project_name"],
+        #     "test_tag": job["test_tag"]
+        # }
+        job["action"] = "notify_user"
+        job["lint_status"] = False
+        job["build_status"] = False
+        job["msg"] = "Couldn't find the Dockerfile at specified git_path"
 
-        queue.put(json.dumps(response), tube="master_tube")
+        queue.put(json.dumps(job), tube="master_tube")
         print "==>Put job on 'master_tube' tube"
         return False
     except BaseException as e:
         print "==> Encountered unexpected error. Dockerfile lint trigger failed"
         print "==> Error: %s" % str(e)
         print "==> Sending Dockerfile linter failure email"
-        response = {
-            "action": "notify_user",
-            "namespace": job["appid"],
-            "notify_email": job["notify_email"],
-            "job_name": job["job_name"],
-            "msg": "Unexpected error while trigger Dockerfile linter",
-            "logs_dir": job["logs_dir"],
-            "lint_status": False,
-            "build_status": False,
-            "project_name": job["project_name"],
-            "test_tag": job["test_tag"]
-        }
+        # response = {
+        #     "action": "notify_user",
+        #     "namespace": job["appid"],
+        #     "notify_email": job["notify_email"],
+        #     "job_name": job["job_name"],
+        #     "msg": "Unexpected error while trigger Dockerfile linter",
+        #     "logs_dir": job["logs_dir"],
+        #     "lint_status": False,
+        #     "build_status": False,
+        #     "project_name": job["project_name"],
+        #     "test_tag": job["test_tag"]
+        # }
+        job["action"] = "notify_user"
+        job["lint_status"] = False
+        job["build_status"] = False
+        job["msg"] = "Unexpected error while trigger Dockerfile linter"
 
-        queue.put(json.dumps(response), tube="master_tube")
+        queue.put(json.dumps(job), tube="master_tube")
         print "==>Put job on 'master_tube' tube"
         return False
     else:
