61fc6a77847cfd4ff0d195593779c232f4aed927
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-11-23 19:52:37 +0530

    Updated fields to all to show all fields in api.

--- container_pipeline/serializers.py
+++ container_pipeline/serializers.py
@@ -5,24 +5,16 @@ from container_pipeline.models.pipeline import Project, Build, BuildPhase
 class ProjectSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Project
-        fields = {
-            'name', 'created', 'last_updated'
-        }
+        fields = "__all__"
 
 
 class BuildSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = Build
-        fields = {
-            'uuid', 'project', 'status', 'logs', 'notification_status', 'start_time',
-            'end_time', 'trigger', 'created', 'last_updated', 'service_debug_log'
-        }
+        fields = "__all__"
 
 
 class BuildPhaseSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = BuildPhase
-        fields = {
-            'build', 'phase', 'status', 'log_file_path', 'start_time', 'end_time',
-            'created', 'last_updated'
-        }
+        fields = "__all__"
