729b8e829b93b62450f19fa225585132fbcd8f6b
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-12-11 10:51:39 +0530

    Updating default and maximum page sizes.

--- container_pipeline/lib/default_settings.py
+++ container_pipeline/lib/default_settings.py
@@ -78,7 +78,9 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
     ],
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
-    'PAGE_SIZE': 50
+    'PAGINATE_BY_PARAM': 'page_size',
+    'PAGE_SIZE': 40,
+    'MAX_PAGINATE_BY': 100
 }
 
 LOGGING = dict(
