fe0c42e7db59ea55f01f996f69768a10b8945ce3
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-12-27 13:49:01 +0530

    Moving constants to lib.

---
+++ container_pipeline/lib/constants.py
@@ -0,0 +1,10 @@
+
+# API Constants begins
+
+API_ENDPOINT = r'^api'
+
+V1_ENDPOINT = API_ENDPOINT + r'/v1'
+V1_PACKAGES_ENDPOINT = V1_ENDPOINT + r'/packages'
+V1_RPM_ENDPOINT = V1_PACKAGES_ENDPOINT + r'/rpms'
+
+# API Constants ends
