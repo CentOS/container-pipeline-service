dc1a67d0948c775bb74cc4a58d77f6de2eaec46a
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-12-27 19:51:01 +0530

    Correcting router urls.

--- container_pipeline/pipeline_api/v1/urls.py
+++ container_pipeline/pipeline_api/v1/urls.py
@@ -7,17 +7,17 @@ from container_pipeline.pipeline_api.v1 import views
 def get_v1_urls():
     urls = []
     router1 = routers.DefaultRouter()
-    router1.register(r'projects', views.ProjectViewSet)
-    router1.register(r'builds', views.BuildViewSet)
-    router1.register(r'build-phases', views.BuildPhaseViewSet)
-    router1.register(r'container-image', views.ContainerImageViewSet)
+    router1.register(r'/projects', views.ProjectViewSet)
+    router1.register(r'/builds', views.BuildViewSet)
+    router1.register(r'/build-phases', views.BuildPhaseViewSet)
+    router1.register(r'/container-image', views.ContainerImageViewSet)
     urls += [
             url(constants.V1_ENDPOINT, include(router1.urls))
         ]
 
     router2 = routers.DefaultRouter()
-    router2.register(r'package', views.PackageViewSet)
-    router2.register(r'repo-info', views.RepoInfoViewSet)
+    router2.register(r'/package', views.PackageViewSet)
+    router2.register(r'/repo-info', views.RepoInfoViewSet)
     urls += [
             url(constants.V1_RPM_ENDPOINT, include(router2.urls))
         ]
