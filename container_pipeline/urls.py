d5e4f3749031453e21e19ec5ca45743314b90470
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-12-27 13:50:45 +0530

    Updating urls according to changes.

--- container_pipeline/urls.py
+++ container_pipeline/urls.py
@@ -1,13 +1,13 @@
 from django.conf.urls import url, include
 from django.contrib import admin
 from rest_framework import routers
-import container_pipeline.api as api
+from container_pipeline.pipeline_api.urls import get_api_urls
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls)
 ]
 
-urlpatterns += api.get_urls()
+urlpatterns += get_api_urls()
 
 urlpatterns += [
     url(r'^api/schema/', include('rest_framework_docs.urls'))
