529cb42144b73d696b9147f1f8ed76276b29f9fa
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2017-12-28 11:50:18 +0530

    Updating to simpler return for now as we have one version.

--- container_pipeline/pipeline_api/urls.py
+++ container_pipeline/pipeline_api/urls.py
@@ -2,6 +2,6 @@ from container_pipeline.pipeline_api.v1.urls import get_v1_urls
 
 
 def get_api_urls():
-    urls = []
-    urls += get_v1_urls()
-    return urls
+    # urls = []
+    # urls += get_v1_urls()
+    return get_v1_urls()
