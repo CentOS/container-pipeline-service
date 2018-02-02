f9e65ab47600c2e7e07b18bcc212e710811cd1c6
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-02-02 10:53:54 +0530

    Updating match pattern where i had missed earlier.

--- testindex/Engine.py
+++ testindex/Engine.py
@@ -68,7 +68,7 @@ class Engine:
         if self._context.environment.verbose:
             print "Processing the data, please wait a while...\n"
 
-        for index_path in glob(self._context.environment.test_index + "/*.yml"):
+        for index_path in glob(self._context.environment.test_index + "/*.yaml"):
             index_file = path.basename(index_path)
 
             status1, status_sublist1 = validators.IndexFormatValidator(self._context, index_path).run()
