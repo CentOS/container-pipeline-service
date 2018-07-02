b41b2613e236400051a30c986cfdc0b9444dfbfd
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-07-02 14:43:21 +0530

    Adding tests for new index CI.
    - Adding basic framework.
    - Adding tests for projects validation and id validation.

---
+++ ci/tests/test_00_unit/test_05_index_ci/indexcibase.py
@@ -0,0 +1,35 @@
+from os import path
+
+from ci.tests.base import BaseTestCase
+import ci.container_index.lib.state as index_ci_state
+
+SETUP_PACKAGES = False
+DUMMY_INDEX_FILE = "./test.yaml"
+
+
+class IndexCIBase(BaseTestCase):
+
+    node = "controller"
+
+    @staticmethod
+    def _print_init_msg(msg):
+        count = len(msg)
+        print("\n")
+        print("=" * count)
+        print(msg)
+        print("=" * count)
+
+    def init_test(self):
+        index_ci_state.init()
+
+    def cleanup_test(self):
+        index_ci_state.clean_up()
+
+    def _setup_test(self):
+        """Setup the requirements for test"""
+        # Setup node info
+        if SETUP_PACKAGES:
+            self.run_cmd("sudo yum -y install epel-release", stream=True)
+            self.run_cmd("sudo yum -y install PyYAML python-networkx",
+                         stream=True)
+        self.setUp()
