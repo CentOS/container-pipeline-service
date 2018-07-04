964ffa847fba5e35edcfa1dadddb69aacb4e429e
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-07-04 14:57:01 +0530

    Adding remaining validations for git clone.

--- ci/tests/test_00_unit/test_05_index_ci/test_01_value_validation/test_00_git_clone_validation_test/__init__.py
+++ ci/tests/test_00_unit/test_05_index_ci/test_01_value_validation/test_00_git_clone_validation_test/__init__.py
@@ -23,3 +23,31 @@ class GitCloneValidationTests(IndexCIBase):
                 DUMMY_INDEX_FILE
             ).validate().success
         )
+
+    def test_02_fails_invalid_git_url_valid_git_branch_with_clone_request(self):
+        self.assertFalse(
+            value_validation.GitCloneValidator(
+                {
+                    CheckKeys.CLONE: True,
+                    FieldKeys.GIT_URL:
+                        "https://github.com/bamachrn/does-not-exist.git",
+                    FieldKeys.GIT_BRANCH: "master",
+                    CheckKeys.STATE: index_ci_state.State()
+                },
+                DUMMY_INDEX_FILE
+            ).validate().success
+        )
+
+    def test_03_fails_valid_git_url_invalid_git_branch_with_clone_request(self):
+        self.assertFalse(
+            value_validation.GitCloneValidator(
+                {
+                    CheckKeys.CLONE: True,
+                    FieldKeys.GIT_URL:
+                        "https://github.com/bamachrn/cccp-python.git",
+                    FieldKeys.GIT_BRANCH: "some-nonexistant-branch",
+                    CheckKeys.STATE: index_ci_state.State()
+                },
+                DUMMY_INDEX_FILE
+            ).validate().success
+        )
