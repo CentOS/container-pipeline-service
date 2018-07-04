c9ca04bb24e4398989d80025c3cbd17a520a90e2
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-07-04 16:47:23 +0530

    Adding base class with common function for file exists validations.

---
+++ ci/tests/test_00_unit/test_05_index_ci/test_01_value_validation/test_01_files_exist_validation/file_exists_base.py
@@ -0,0 +1,25 @@
+from ci.tests.test_00_unit.test_05_index_ci.indexcibase import IndexCIBase, \
+    DUMMY_INDEX_FILE
+from ci.container_index.lib.constants import *
+import ci.container_index.lib.checks.value_validation as value_validation
+import ci.container_index.lib.state as index_ci_state
+from uuid import uuid4
+from os import path, mkdir, utime
+
+
+class FileExistsBaseTest(IndexCIBase):
+
+    def setup_mock_location(self, file_names):
+        s = index_ci_state.State()
+        mock_loc = path.join(s.state_mock, str(uuid4()))
+        if not path.exists(mock_loc):
+            mkdir(mock_loc)
+        for f, d in file_names.iteritems():
+            p = path.join(mock_loc, f)
+            if not d:
+                with open(p, "a"):
+                    utime(p, None)
+            else:
+                with open(p, "w+") as fl:
+                    fl.write(d)
+        return s, mock_loc
