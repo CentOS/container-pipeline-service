c0efebc1dd14bb70bc63ed81c1fd92b9cb763535
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-07-12 12:59:21 +0530

    Rewiring index ci to run the new validations.

--- ci/cccp_ci_container_index.sh
+++ ci/cccp_ci_container_index.sh
@@ -6,8 +6,11 @@ sshopts="-t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l root"
 ssh_cmd="ssh $sshopts $CICO_hostname"
 $ssh_cmd "yum -y install epel-release && yum -y install rsync git PyYAML python-networkx"
 rsync -e "ssh $sshopts" -Ha $(pwd)/ $CICO_hostname:payload
-$ssh_cmd git clone https://github.com/CentOS/container-pipeline-service.git 
-$ssh_cmd /usr/bin/python container-pipeline-service/testindex/__init__.py  -vi payload/index.d
+#$ssh_cmd git clone https://github.com/CentOS/container-pipeline-service.git 
+$ssh_cmd git clone https://github.com/mohammedzee1000/container-pipeline-service.git
+$ssh_cmd git fetch --all
+$ssh_cmd git checkout origin/index_ci_refac_6
+$ssh_cmd /usr/bin/python container-pipeline-service/ci/container_index/run.py  -vi ./payload
 rtn_code=$?
 cico node done $CICO_ssid
 exit $rtn_code
