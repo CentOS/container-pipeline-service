923a80c0951e1ccaf501c2fd621ca87cc1e199b3
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-07-13 14:27:06 +0530

    Removing references that pull different branch.

--- ci/cccp_ci_container_index.sh
+++ ci/cccp_ci_container_index.sh
@@ -6,10 +6,7 @@ sshopts="-t -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -l root"
 ssh_cmd="ssh $sshopts $CICO_hostname"
 $ssh_cmd "yum -y install epel-release && yum -y install rsync git PyYAML python-networkx"
 rsync -e "ssh $sshopts" -Ha $(pwd)/ $CICO_hostname:payload
-#$ssh_cmd git clone https://github.com/CentOS/container-pipeline-service.git 
-$ssh_cmd git clone https://github.com/mohammedzee1000/container-pipeline-service.git
-$ssh_cmd git fetch --all
-$ssh_cmd git checkout origin/index_ci_refac_6
+$ssh_cmd git clone https://github.com/CentOS/container-pipeline-service.git 
 $ssh_cmd /usr/bin/python container-pipeline-service/ci/container_index/run.py  -vi ./payload
 rtn_code=$?
 cico node done $CICO_ssid
