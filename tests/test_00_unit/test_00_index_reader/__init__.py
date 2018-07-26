4cf436f2b3b8225e83922d61440673d4c4b8153c
Navid Shaikh <nshaikh@redhat.com>
2018-07-26 19:12:56 +0530

    Adds unittests for desired_tag and depends_on field of index_reader

--- tests/test_00_unit/test_00_index_reader/__init__.py
+++ tests/test_00_unit/test_00_index_reader/__init__.py
@@ -8,22 +8,23 @@ class TestProject(unittest.TestCase):
     Test the Project class representing an
     object in container pipeline
     """
+
     def setUp(self):
         self.entry = {
-                "id": "1",
-                "app-id": "foo",
-                "job-id": "bar",
-                "desired-tag": "latest",
-                "git-branch": "master",
-                "git-url": "https://github.com/someurl",
-                "git-path": "foo/",
-                "target-file": "Dockerfile",
-                "build-context": "./",
-                "notify-email": "abc@example/com",
-                "depends-on": ["centos/centos:latest", "centos/centos:7"],
-                "prebuild-script": "hooks/script.sh",
-                "prebuild-context": "/",
-                }
+            "id": "1",
+            "app-id": "foo",
+            "job-id": "bar",
+            "desired-tag": "latest",
+            "git-branch": "master",
+            "git-url": "https://github.com/someurl",
+            "git-path": "foo/",
+            "target-file": "Dockerfile",
+            "build-context": "./",
+            "notify-email": "abc@example/com",
+            "depends-on": ["centos/centos:latest", "centos/centos:7"],
+            "prebuild-script": "hooks/script.sh",
+            "prebuild-context": "/",
+        }
         self.namespace = "ccp"
 
     def test_load_entries(self):
@@ -31,7 +32,36 @@ class TestProject(unittest.TestCase):
         Tests creating Project class object which loads
         entries
         """
+        index_reader.Project(self.entry, self.namespace)
+
+    def test_desired_tag(self):
+        """
+        Tests desired tag processing and defaults
+        """
+        # test a custom value
+        self.entry["desired-tag"] = "release"
         project = index_reader.Project(self.entry, self.namespace)
+        self.assertEqual(project.desired_tag, "release")
+
+        # test empty value
+        self.entry["desired-tag"] = ""
+        project = index_reader.Project(self.entry, self.namespace)
+        self.assertEqual(project.desired_tag, "latest")
+
+    def test_depends_on(self):
+        """
+        Tests depends_on field processing and defaults
+        """
+        # test the default value
+        project = index_reader.Project(self.entry, self.namespace)
+        # test if multiple/list of depends-on is converted to string
+        self.assertIsInstance(
+            project.depends_on,
+            str
+        )
+
+        # test if commas are included with multiple values of depends_on
+        self.assertIn(",", project.depends_on)
 
 
 if __name__ == "__main__":
