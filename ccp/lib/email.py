3c63d8afbbf48c3e64098c9aadff04ce9487e514
Navid Shaikh <nshaikh@redhat.com>
2018-08-19 14:14:10 +0530

    Adds email module  in ccp library utilities
      Module has Email class with email utilities.

---
+++ ccp/lib/email.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+
+from ccp.lib.command import run_cmd
+
+
+class SendEmail(object):
+    """
+    This class has related methods for sending email
+    """
+    def escape_text(self, text):
+        """
+        Given text, escapes newlines and tabs
+        """
+        return text.replace("\n", "\\n").replace("\t", "\\t")
+
+    def email(self,
+              smtp_server, sub, body,
+              from_add, to_adds, cc_adds=[]):
+        """
+        Send email using given details
+        smtp_server: URL of smtp server
+        sub: Subject of email
+        body: Body of email
+        from_add: From address
+        to_adds: A list of to addresses
+        cc_adds: (optional) A list addresses to mark in Cc
+        """
+        command = """\
+echo -e '{body}' | mail -r {from_address} {cc_opts} -S \
+smtp={smtp_server} -s "{subject}" {to_addresses}"""
+
+        # it would return '' for [] / no cc_add
+        cc_opts = " ".join(map("-c {0}".format(cc_adds)))
+
+        # to addresses
+        to_addresses = " ".join(to_adds)
+
+        # escape the \n and \t characters
+        body = self.escape_text(body)
+
+        command.format(
+            body=body,
+            from_address=from_add,
+            cc_opts=cc_opts,
+            smtp_server=smtp_server,
+            subject=sub,
+            to_addresses=to_addresses)
+
+        # send email
+        run_cmd(command, shell=True)
+        print ("Email sent to {}".format(to_adds))
