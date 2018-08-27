5f62ab903558cad8b558e87543f602ecce1ed167
Bamacharan Kundu <bamachrn@gmail.com>
2018-08-27 10:07:20 +0530

    Removed old code base in faovour of new one

--- ci/setup_env.sh
+++
@@ -1,8 +0,0 @@
-if [ -f env.properties ]; then
-    rm env.properties
-fi
-touch env.properties
-echo "PYTHONPATH=$(pwd):$PYTHONPATH" >> env.properties
-
-bash provisions/utils/gencert.sh registry.centos.org || true
-echo -e  'y\n'| ssh-keygen -t rsa -N "" -f provisions/jenkins.key
--- container_pipeline/lib/settings.py
+++
@@ -1,1 +0,0 @@
-from container_pipeline.lib.default_settings import *  # noqa
