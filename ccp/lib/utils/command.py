0d37e4a4201d525b23809d2156bcbf2dcf4388be
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-10-12 13:12:40 +0530

    Updating to have 2 correctly named run_cmd

--- ccp/lib/utils/command.py
+++ ccp/lib/utils/command.py
@@ -7,14 +7,28 @@ def run_command(cmd, shell=False):
     """
     Runs a shell command
     :param cmd: The command that needs to be run.
+    :type cmd: s
     :param shell: Default False: Whether to run raw shell commands with '|' and
     redirections
-    :return: Command output
+    :return: Command output and err
     :raises subprocess.CalledProcessError
-    :raises ccp.lib.exceptions.CommandOutputError
     """
     p = subprocess.Popen(cmd, shell=shell)
     out, err = p.communicate()
+    return out, err
+
+
+def run_command_exception_on_stderr(cmd, shell=False):
+    """
+    Runs a shell command, raised exception if stderr
+    :param cmd: The command that needs to be run.
+    :param shell: Default False: Whether to run raw shell commands with '|' and
+    redirections
+    :return: Command output and err
+    :raises subprocess.CalledProcessError
+    :raises ccp.lib.exceptions.CommandOutputError
+    """
+    out, err = run_command(cmd, shell=shell)
     if err:
         raise CommandOutputError(err)
     return out
