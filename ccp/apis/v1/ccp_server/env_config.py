095f7459af7238f9ecb3ff8b35163a6163b92901
Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>
2018-11-22 15:34:59 +0530

    Implementing namespaces endpoint.
    Signed-off-by: Mohammed Zeeshan Ahmed <mohammed.zee1000@gmail.com>

--- ccp/apis/v1/ccp_server/env_config.py
+++ ccp/apis/v1/ccp_server/env_config.py
@@ -4,3 +4,4 @@ import os
 OPENSHIFT_VERSION = os.environ.get("OPENSHIFT_VERSION")
 OPENSHIFT_URL = os.environ.get("OPENSHIFT_SERVER")
 JENKINS_URL = os.environ.get("JENKINS_URL")
+SERVICE_ACCOUNT_SECRET_MOUNT_PATH = "/run/secrets/kubernetes.io/serviceaccount"
