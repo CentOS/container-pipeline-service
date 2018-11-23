e5d2f0de7e47d87fe26d0935206cefdcbb408c5a
Navid Shaikh <nshaikh@redhat.com>
2018-11-23 12:57:54 +0530

    Updates container-capabilities-scanner to use BaseScanner

--- ccp/scanning/container-capabilities.py
+++ ccp/scanning/container-capabilities.py
@@ -5,64 +5,78 @@
 # the check args method is taken from
 # github.com/projectatomic/atomic/blob/master/Atomic/backends/_docker.py
 
+
+from scanners.base_scanner import BaseScanner
+
 import sys
 
 
-def check_args(cmd):
+class ContainerCapabilitiesScanner(BaseScanner):
     """
-    Check the arguments of the docker run command and provide feedback
+    Container capabilities scanner
     """
-    found_sec_arg = False
-    security_args = {
-        '--privileged':
-            'This container runs without separation and should be '
-            'considered the same as root on your system.',
-        '--cap-add':
-            'Adding capabilities to your container could allow processes '
-            'from the container to break out onto your host system.',
-        '--security-opt label:disable':
-            'Disabling label separation turns off tools like SELinux and '
-            'could allow processes from the container to break out onto '
-            'your host system.',
-        '--security-opt label=disable':
-            'Disabling label separation turns off tools like SELinux and '
-            'could allow processes from the container to break out onto '
-            'your host system.',
-        '--net=host':
-            'Processes in this container can listen to ports (and '
-            'possibly rawip traffic) on the host\'s network.',
-        '--pid=host':
-            'Processes in this container can see and interact with all '
-            'processes on the host and disables SELinux within the '
-            'container.',
-        '--ipc=host':
-            'Processes in this container can see and possibly interact '
-            'with all semaphores and shared memory segments on the host '
-            'as well as disables SELinux within the container.'
-    }
+    NAME = "container-capabilities-scanner"
+    DESCRIPTION = ("Inspects the run label of image and guides about "
+                   "recommended uses for run flags defined in label.")
 
-    for sec_arg in security_args:
-        if sec_arg in cmd:
-            if not found_sec_arg:
-                print("\nThis container uses privileged "
-                      "security switches:")
-            print("\nINFO: {0}\n\t{1}".format(sec_arg, security_args[sec_arg]))
-            found_sec_arg = True
-    if found_sec_arg:
-        print("\nFor more information on these switches and their "
-              "security implications, consult the manpage for "
-              "'docker run'.\n")
+    def __init__(self):
+        super(ContainerCapabilitiesScanner, self).__init__()
 
+    def check_args(self, cmd):
+        """
+        Check the arguments of the docker run command and provide feedback
+        """
+        found_sec_arg = False
+        security_args = {
+            '--privileged':
+                'This container runs without separation and should be '
+                'considered the same as root on your system.',
+            '--cap-add':
+                'Adding capabilities to your container could allow processes '
+                'from the container to break out onto your host system.',
+            '--security-opt label:disable':
+                'Disabling label separation turns off tools like SELinux and '
+                'could allow processes from the container to break out onto '
+                'your host system.',
+            '--security-opt label=disable':
+                'Disabling label separation turns off tools like SELinux and '
+                'could allow processes from the container to break out onto '
+                'your host system.',
+            '--net=host':
+                'Processes in this container can listen to ports (and '
+                'possibly rawip traffic) on the host\'s network.',
+            '--pid=host':
+                'Processes in this container can see and interact with all '
+                'processes on the host and disables SELinux within the '
+                'container.',
+            '--ipc=host':
+                'Processes in this container can see and possibly interact '
+                'with all semaphores and shared memory segments on the host '
+                'as well as disables SELinux within the container.'
+        }
 
-def run_scan(command):
-    """
-    Runs the container capabilities scan and prints error message
-    if provided command is empty
-    """
-    if not command or command == "null":
-        print ("\n RUN label is not available in image under test.")
-    else:
-        check_args(command)
+        for sec_arg in security_args:
+            if sec_arg in cmd:
+                if not found_sec_arg:
+                    print("\nThis container uses privileged "
+                          "security switches:")
+                print("\nINFO: {0}\n\t{1}".format(
+                    sec_arg, security_args[sec_arg]))
+                found_sec_arg = True
+        if found_sec_arg:
+            print("\nFor more information on these switches and their "
+                  "security implications, consult the manpage for "
+                  "'docker run'.\n")
+
+    def run(self, command):
+        """
+        Runs the container capabilities scan and prints error message
+        if provided command is empty
+        """
+        if not command or command == "null":
+            print ("\n RUN label is not available in image under test.")
+        else:
+            self.check_args(command)
 
 
 if __name__ == "__main__":
@@ -74,7 +88,8 @@ if __name__ == "__main__":
 
     try:
         cli_arg = sys.argv[1].strip()
-        run_scan(cli_arg)
+        cc_scanner = ContainerCapabilitiesScanner()
+        cc_scanner.run(cli_arg)
     except Exception as e:
         print ("Error occurred in Container Capabilities scanner execution.")
         print ("Error: {0}".format(e))
