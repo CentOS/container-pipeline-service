ac114f0a47d2cd7f39db0659ca00779ef1851e40
Bamacharan Kundu <bamachrn@gmail.com>
2019-02-06 10:34:05 +0530

    added missing lib directory

---
+++ ci/container_index/lib/checks/value_validation.py
@@ -0,0 +1,157 @@
+"""
+This file contains validators that check for validity of values provided in the
+index
+"""
+from os import path
+
+from ci.container_index.lib.checks.basevalidation import \
+    OptionalClonedValidator, CCCPYamlValidator
+from ci.container_index.lib.constants import *
+
+
+class GitCloneValidator(OptionalClonedValidator):
+    """
+    Validates if git repo is clonable and requisite branch can be checked out.
+    """
+    def __init__(self, validation_data, file_name):
+        super(GitCloneValidator, self).__init__(validation_data, file_name)
+
+    def _validate_after(self):
+        self.message.title = "Git Clone Validator"
+        if not self.clone_location:
+            self._invalidate("Failed to clone git repo or checkout git branch")
+            return
+
+
+class CCCPYamlExistsValidator(CCCPYamlValidator):
+    """
+    Validates if cccp yaml file exists or not
+    """
+    def __init__(self, validation_data, file_name):
+        super(CCCPYamlExistsValidator, self).__init__(
+            validation_data, file_name
+        )
+        self.message.title = "CCCP Yaml Exists Validator"
+
+
+class TargetFileExistsValidator(OptionalClonedValidator):
+    """
+    Validates if target-file exists at specified location
+    """
+    def __init__(self, validation_data, file_name):
+        super(TargetFileExistsValidator, self).__init__(
+            validation_data, file_name
+        )
+
+    def _validate_after(self):
+        self.message.title = "Target File Exists Validator"
+        if FieldKeys.PREBUILD_SCRIPT in self.validation_data:
+            self._warn("Skipping target file check as prebuild step exists")
+            return
+        if (not path.exists(
+            str.format(
+                "{}/{}/{}",
+                self.clone_location,
+                self.validation_data.get(FieldKeys.GIT_PATH),
+                self.validation_data.get(FieldKeys.TARGET_FILE)
+             ))):
+            self._invalidate(
+                str.format(
+                    "Target File {} does not exist, at the path {}",
+                    self.validation_data.get(FieldKeys.TARGET_FILE),
+                    self.validation_data.get(FieldKeys.GIT_PATH)
+                )
+            )
+            return
+
+
+class PreBuildExistsValidator(OptionalClonedValidator):
+    """
+    Checks if prebuild data is valid.
+    """
+    def __init__(self, validation_data, file_name):
+        super(PreBuildExistsValidator, self).__init__(
+            validation_data, file_name
+        )
+
+    def _validate_after(self):
+        self.message.title = "Prebuild Exists Validator"
+        if FieldKeys.PREBUILD_SCRIPT not in self.validation_data:
+            return
+        if (FieldKeys.PREBUILD_SCRIPT in self.validation_data and
+                FieldKeys.PREBUILD_CONTEXT not in self.validation_data):
+            self._invalidate(
+                "Prebuild script is provided, but no prebuild context"
+            )
+            return
+
+        if (not path.exists(
+                str.format(
+                    "{}/{}",
+                    self.clone_location,
+                    self.validation_data.get(FieldKeys.PREBUILD_CONTEXT)
+                )
+        )):
+            self._invalidate(
+                str.format(
+                    "Prebuild context does not exist at {}",
+                    self.validation_data.get(FieldKeys.PREBUILD_CONTEXT)
+                )
+            )
+        if (not path.exists(
+                str.format(
+                    "{}/{}/{}",
+                    self.clone_location,
+                    self.validation_data.get(FieldKeys.PREBUILD_CONTEXT),
+                    self.validation_data.get(FieldKeys.PREBUILD_SCRIPT)
+                )
+        )):
+            self._invalidate(
+                str.format(
+                    "Prebuild script does not exist at {}/{}",
+                    self.validation_data.get(FieldKeys.PREBUILD_CONTEXT),
+                    self.validation_data.get(FieldKeys.PREBUILD_SCRIPT)
+                )
+            )
+
+
+class JobIDMatchesIndex(CCCPYamlValidator):
+    """
+    Checks if Job id in cccp yaml is same as in index
+    """
+
+    def __init__(self, validation_data, file_name):
+        super(JobIDMatchesIndex, self).__init__(validation_data, file_name)
+        self.message.title = "CCCP Job id matches index"
+
+    def _validate_cccp_yaml(self):
+        # TODO : Remove this once centos cccp yaml is fixed.
+        app_id = self.validation_data.get(FieldKeys.APP_ID)
+        job_id = self.validation_data.get(FieldKeys.JOB_ID)
+        if app_id and job_id:
+            if ((app_id == "centos" or app_id == "library")
+               and (job_id == "centos" or job_id == "nginx-110-centos7")):
+                self._warn(
+                    "CentOS image detected, skipping for now until fix is"
+                    " done to repository"
+                )
+                return
+            if app_id == "fabric8-ui" or app_id == "fabric8":
+                self._warn(
+                    "Skipping check on fabric8-ui for noew"
+                )
+                return
+            if app_id == "pipeline-images":
+                self._warn(
+                    "Skipping for pipeline-images for now as they are"
+                    " an exception case"
+                )
+                return
+        # END
+        cccp_jid = self._cccp_yaml_data.get(FieldKeys.JOB_ID)
+        if not cccp_jid:
+            self._invalidate("job-id must be present in the cccp yaml file")
+            return
+        if cccp_jid and cccp_jid != self.validation_data.get(FieldKeys.JOB_ID):
+            self._invalidate("job-id does not match value provided in index.")
+            return
